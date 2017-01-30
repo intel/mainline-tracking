0885a4899b63829d55620a04c0c65e1971767dae
Pawse, GuruprasadX <guruprasadx.pawse@intel.com>
2017-01-30 20:12:52 +0530

    ASoC: Intel: Skylake: add sysfs files for firmware modules
    This patch adds sysfs files for firmware modules.
    
    Below is the structure of sysfs files created:
    /sys/bus/pci/devices/<DOMAIN:BUS:DEVICE.FUNCTION>/dsp/modules/<UUID>
    	|---id:			module id
    	|---hash:		module hash
    	|---loaded:		module state loaded/unloaded
    
    Change-Id: Ia097a3cc1409a33b2a82b1d1cdc634fb4b0eee90
    Signed-off-by: Pawse, GuruprasadX <guruprasadx.pawse@intel.com>
    
    Reviewed-by: R, Dharageswari <dharageswari.r@intel.com>
    Reviewed-by: Kp, Jeeja <jeeja.kp@intel.com>
    Reviewed-by: Prodduvaka, Leoni 
    Tested-by: Sm, Bhadur A <bhadur.a.sm@intel.com>
    
    ASoC: Intel: Skylake: Fix Audio memory leakage at boot time
    
    Fix kmemleak issues in skl_module_sysfs_init functions by,
    instead of allocating memory, we use local variable of type
    "char array" and snprintf to write uuid.
    
    kmemleak backtrace:
    
    unreferenced object 0xffff880267946e00 (size 64):
      comm "insmod", pid 2340, jiffies 4294672904 (age 92.133s)
      hex dump (first 32 bytes):
        36 36 42 34 34 30 32 44 2d 42 34 36 38 2d 34 32  66B4402D-B468-42
        46 32 2d 38 31 41 37 2d 42 33 37 31 32 31 38 36  F2-81A7-B3712186
      backtrace:
        [<ffffffff81ae64ca>] kmemleak_alloc+0x4a/0xa0
        [<ffffffff812006b4>] __kmalloc_track_caller+0x124/0x200
        [<ffffffff81404126>] kvasprintf+0x66/0xd0
        [<ffffffff8140427e>] kasprintf+0x4e/0x70
        [<ffffffffc035dafa>] skl_module_sysfs_init+0x13a/0x260 [snd_soc_skl_ipc]
        [<ffffffffc036f906>] skl_platform_soc_probe+0x246/0x480 [snd_soc_skl]
        [<ffffffff818f5a0c>] snd_soc_platform_drv_probe+0x1c/0x20
        [<ffffffff818f9307>] soc_probe_component+0x227/0x3c0
        [<ffffffff818fb857>] snd_soc_register_card+0x687/0xf00
        [<ffffffffc03910ba>] broxton_tdf8532_audio_probe+0x3a/0x3d [snd_soc_sst_bxt_tdf8532]
        [<ffffffff8164283e>] platform_drv_probe+0x3e/0xa0
        [<ffffffff8164066f>] driver_probe_device+0xef/0x410
        [<ffffffff81640a37>] __driver_attach+0xa7/0xf0
        [<ffffffff8163e130>] bus_for_each_dev+0x70/0xc0
        [<ffffffff8163fefe>] driver_attach+0x1e/0x20
        [<ffffffff8163f987>] bus_add_driver+0x1c7/0x270

--- sound/soc/intel/skylake/skl-sst-ipc.h
+++ sound/soc/intel/skylake/skl-sst-ipc.h
@@ -121,6 +121,9 @@ struct skl_sst {
 
 	/* Callback to update dynamic clock and power gating registers */
 	void (*clock_power_gating)(struct device *dev, bool enable);
+
+	/* sysfs for module info */
+	struct skl_sysfs_tree *sysfs_tree;
 };
 
 struct skl_ipc_init_instance_msg {
