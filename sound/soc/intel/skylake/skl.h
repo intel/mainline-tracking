a9a059d267c0e2d15350dde4a086fc377b2fdb61
Diwakar, Praveen <praveen.diwakar@intel.com>
2017-06-06 21:04:21 +0530

    ASoC: Intel: Skylake: Create SSP BE dais dynamically
    This patch creates BE SSP dai dynamically, by getting SSP
    link information from NHLT.
    
    Change-Id: I2b6e45125a3fbd1e7f155efe86b5fb1a983c0f41
    Signed-off-by: Diwakar, Praveen <praveen.diwakar@intel.com>
    Reviewed-on: 
    Reviewed-by: Kale, Sanyog R <sanyog.r.kale@intel.com>
    Tested-by: Sm, Bhadur A <bhadur.a.sm@intel.com>

--- sound/soc/intel/skylake/skl.h
+++ sound/soc/intel/skylake/skl.h
@@ -57,6 +57,11 @@ struct skl_fw_config {
 	struct skl_astate_config *astate_cfg;
 };
 
+struct ep_group_cnt {
+	int cnt;
+	int *vbus_id;
+};
+
 struct skl_dev {
 	struct hda_bus hbus;
 	struct pci_dev *pci;
@@ -142,6 +147,8 @@ struct skl_dev {
 	/* sysfs for module info */
 	struct skl_sysfs_tree *sysfs_tree;
 
+	struct ep_group_cnt grp_cnt;
+
 	/* probe stream management */
 	struct hdac_ext_stream *extractor;
 	unsigned int num_probe_streams;
@@ -176,6 +183,7 @@ int skl_platform_unregister(struct device *dev);
 int skl_platform_register(struct device *dev);
 
 int skl_get_nhlt_version(struct device *dev);
+void skl_nhlt_get_ep_cnt(struct skl_dev *skl, int link_type);
 struct nhlt_acpi_table *skl_nhlt_init(struct device *dev);
 void skl_nhlt_free(struct nhlt_acpi_table *addr);
 struct nhlt_specific_cfg *skl_get_ep_blob(struct skl_dev *skl, u32 instance,
