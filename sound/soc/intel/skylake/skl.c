f535b6111cf4f950124ba6938bbdbf871616a6a5
Paul, Subhankar <subhankar.paul@intel.com>
2017-08-08 22:46:45 +0530

    ASoC: Intel: Skylake: Add support for module notifications
    Firmware modules can send asynchronous notification to driver
    with event data as payload. Add support for notifying user of such
    asynchronous notifications from firmware modules by adding
    kcontrols. These kcontrols have the module event data that needs to be
    sent to user.
    
    Change-Id: If204e275a9613c769cf00fe632e45b174bd2fa2f
    Signed-off-by: Mohit Sinha <mohit.sinha@intel.com>
    Reviewed-on: 
    Reviewed-by: Shaik, Kareem M <kareem.m.shaik@intel.com>
    Reviewed-by: Kesapragada, Pardha Saradhi <pardha.saradhi.kesapragada@intel.com>
    Reviewed-by: Paul, Subhankar <subhankar.paul@intel.com>
    Reviewed-by: Koul, Vinod <vinod.koul@intel.com>
    Reviewed-by: audio_build 
    Tested-by: Avati, Santosh Kumar <santosh.kumar.avati@intel.com>

--- sound/soc/intel/skylake/skl.c
+++ sound/soc/intel/skylake/skl.c
@@ -29,6 +29,7 @@
 #include "skl.h"
 #include "skl-sst-dsp.h"
 #include "skl-sst-ipc.h"
+#include "skl-topology.h"
 #if IS_ENABLED(CONFIG_SND_SOC_INTEL_SKYLAKE_HDAUDIO_CODEC)
 #include "../../../soc/codecs/hdac_hda.h"
 #endif
@@ -1177,7 +1178,8 @@ static void skl_remove(struct pci_dev *pci)
 	struct skl_dev *skl = bus_to_skl(bus);
 
 	cancel_work_sync(&skl->probe_work);
-
+	skl_delete_notify_kctl_list(skl);
+	release_firmware(skl->tplg);
 	pm_runtime_get_noresume(&pci->dev);
 
 	/* codec removal, invoke bus_device_remove */
