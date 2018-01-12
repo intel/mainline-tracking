7c5c8d2038f06486446db9027aafd745ef2a58bc
Yu-cheng Yu <yu-cheng.yu@intel.com>
2018-01-12 15:04:54 -0800

    mm: Introduce VM_SHSTK for shadow stack memory
    A Shadow Stack PTE must be read-only and have _PAGE_DIRTY set.  However,
    read-only and Dirty PTEs also exist for copy-on-write (COW) pages.  These
    two cases are handled differently for page faults.  Introduce VM_SHSTK to
    track shadow stack VMAs.
    
    Signed-off-by: Yu-cheng Yu <yu-cheng.yu@intel.com>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    
    v9:
    - Add VM_SHSTK case to arch_vma_name().
    - Revise the commit log to explain why adding a new VM flag.

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -302,11 +302,13 @@ extern unsigned int kobjsize(const void *objp);
 #define VM_HIGH_ARCH_BIT_2	34	/* bit only usable on 64-bit architectures */
 #define VM_HIGH_ARCH_BIT_3	35	/* bit only usable on 64-bit architectures */
 #define VM_HIGH_ARCH_BIT_4	36	/* bit only usable on 64-bit architectures */
+#define VM_HIGH_ARCH_BIT_5	37	/* bit only usable on 64-bit architectures */
 #define VM_HIGH_ARCH_0	BIT(VM_HIGH_ARCH_BIT_0)
 #define VM_HIGH_ARCH_1	BIT(VM_HIGH_ARCH_BIT_1)
 #define VM_HIGH_ARCH_2	BIT(VM_HIGH_ARCH_BIT_2)
 #define VM_HIGH_ARCH_3	BIT(VM_HIGH_ARCH_BIT_3)
 #define VM_HIGH_ARCH_4	BIT(VM_HIGH_ARCH_BIT_4)
+#define VM_HIGH_ARCH_5	BIT(VM_HIGH_ARCH_BIT_5)
 #endif /* CONFIG_ARCH_USES_HIGH_VMA_FLAGS */
 
 #ifdef CONFIG_ARCH_HAS_PKEYS
@@ -338,6 +340,12 @@ extern unsigned int kobjsize(const void *objp);
 # define VM_ARCH_CLEAR	VM_ARM64_BTI
 #elif !defined(CONFIG_MMU)
 # define VM_MAPPED_COPY	VM_ARCH_1	/* T if mapped copy of data (nommu mmap) */
+#endif
+
+#ifdef CONFIG_X86_INTEL_SHADOW_STACK_USER
+# define VM_SHSTK	VM_HIGH_ARCH_5
+#else
+# define VM_SHSTK	VM_NONE
 #endif
 
 #ifndef VM_GROWSUP
