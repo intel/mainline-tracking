132fb586f3a614c2ad6a8db59b18b14f7fb6fe96
Yu-cheng Yu <yu-cheng.yu@intel.com>
2018-04-12 09:32:59 -0700

    x86/mm: Change _PAGE_DIRTY to _PAGE_DIRTY_HW
    Before introducing _PAGE_COW for non-hardware memory management purposes in
    the next patch, rename _PAGE_DIRTY to _PAGE_DIRTY_HW and _PAGE_BIT_DIRTY to
    _PAGE_BIT_DIRTY_HW to make meanings more clear.  There are no functional
    changes from this patch.
    
    Signed-off-by: Yu-cheng Yu <yu-cheng.yu@intel.com>
    Reviewed-by: Kees Cook <keescook@chromium.org>
    Reviewed-by: Dave Hansen <dave.hansen@intel.com>

--- arch/x86/kvm/vmx/vmx.c
+++ arch/x86/kvm/vmx/vmx.c
@@ -3574,7 +3574,7 @@ static int init_rmode_identity_map(struct kvm *kvm)
 	/* Set up identity-mapping pagetable for EPT in real mode */
 	for (i = 0; i < PT32_ENT_PER_PAGE; i++) {
 		tmp = (i << 22) + (_PAGE_PRESENT | _PAGE_RW | _PAGE_USER |
-			_PAGE_ACCESSED | _PAGE_DIRTY | _PAGE_PSE);
+			_PAGE_ACCESSED | _PAGE_DIRTY_HW | _PAGE_PSE);
 		r = kvm_write_guest_page(kvm, identity_map_pfn,
 				&tmp, i * sizeof(tmp), sizeof(tmp));
 		if (r < 0)
