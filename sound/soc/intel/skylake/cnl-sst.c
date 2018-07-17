cc6d1d547fd7b1f8cf065938927999254552d2b2
Cezary Rojewski <cezary.rojewski@intel.com>
2018-07-17 17:06:34 +0200

    ASoC: Intel: Allow for firmware load retry
    Due to unconditional initial timeouts, firmware may fail to load
    during its initialization. This issue cannot be resolved on driver
    side but has to be accounted for nonetheless.
    
    Default firmware load retry count is set to 3.
    
    Change-Id: Idd12483e07633d1f809cdc4363fb51f5217233ad
    Signed-off-by: Cezary Rojewski <cezary.rojewski@intel.com>
    Reviewed-on: 
    Reviewed-by: Slawinski, AmadeuszX <amadeuszx.slawinski@intel.com>
    Reviewed-by: Lewandowski, Gustaw <gustaw.lewandowski@intel.com>
    Tested-by: Lewandowski, Gustaw <gustaw.lewandowski@intel.com>

--- sound/soc/intel/skylake/cnl-sst.c
+++ sound/soc/intel/skylake/cnl-sst.c
@@ -117,13 +117,13 @@ static int cnl_load_base_firmware(struct sst_dsp *ctx)
 	struct firmware stripped_fw;
 	struct skl_dev *cnl = ctx->thread_context;
 	struct sst_pdata *pdata = ctx->pdata;
-	int ret;
+	int ret, i;
 
 	if (!pdata->fw) {
 		ret = request_firmware(&pdata->fw, ctx->fw_name, ctx->dev);
 		if (ret < 0) {
 			dev_err(ctx->dev, "request firmware failed: %d\n", ret);
-			goto cnl_load_base_firmware_failed;
+			return ret;
 		}
 	}
 
@@ -131,25 +131,33 @@ static int cnl_load_base_firmware(struct sst_dsp *ctx)
 		ret = snd_skl_parse_manifest(ctx, pdata->fw,
 						CNL_ADSP_FW_HDR_OFFSET, 0);
 		if (ret < 0)
-			goto cnl_load_base_firmware_failed;
+			goto load_base_firmware_failed;
 	}
 
 	stripped_fw.data = pdata->fw->data;
 	stripped_fw.size = pdata->fw->size;
 	skl_dsp_strip_extended_manifest(&stripped_fw);
 
-	ret = cnl_prepare_fw(ctx, stripped_fw.data, stripped_fw.size);
-	if (ret < 0) {
-		dev_err(ctx->dev, "prepare firmware failed: %d\n", ret);
-		goto cnl_load_base_firmware_failed;
+	ret = -ENOEXEC;
+	for (i = 0; i < SST_FW_INIT_RETRY && ret < 0; i++) {
+		ret = cnl_prepare_fw(ctx, stripped_fw.data, stripped_fw.size);
+		if (ret < 0) {
+			dev_dbg(ctx->dev, "prepare firmware failed: %d\n", ret);
+			continue;
+		}
+
+		dev_dbg(ctx->dev, "ROM loaded successfully on iteration %d.\n", i);
+
+		ret = sst_transfer_fw_host_dma(ctx, ret);
+		if (ret < 0) {
+			dev_dbg(ctx->dev, "transfer firmware failed: %d\n", ret);
+			cnl_dsp_disable_core(ctx, SKL_DSP_CORE0_MASK);
+		}
 	}
 
-	ret = sst_transfer_fw_host_dma(ctx, ret);
-	if (ret < 0) {
-		dev_err(ctx->dev, "transfer firmware failed: %d\n", ret);
-		cnl_dsp_disable_core(ctx, SKL_DSP_CORE0_MASK);
-		goto cnl_load_base_firmware_failed;
-	}
+	if (ret < 0)
+		goto load_base_firmware_failed;
+	dev_dbg(ctx->dev, "Firmware download successful.\n");
 
 	ret = wait_event_timeout(cnl->boot_wait, cnl->boot_complete,
 				 msecs_to_jiffies(SKL_IPC_BOOT_MSECS));
@@ -157,14 +165,15 @@ static int cnl_load_base_firmware(struct sst_dsp *ctx)
 		dev_err(ctx->dev, "FW ready timed-out\n");
 		cnl_dsp_disable_core(ctx, SKL_DSP_CORE0_MASK);
 		ret = -EIO;
-		goto cnl_load_base_firmware_failed;
+		goto load_base_firmware_failed;
 	}
 
 	cnl->fw_loaded = true;
 
 	return 0;
 
-cnl_load_base_firmware_failed:
+load_base_firmware_failed:
+	dev_err(ctx->dev, "Firmware load failed: %d.\n", ret);
 	release_firmware(pdata->fw);
 	pdata->fw = NULL;
 
