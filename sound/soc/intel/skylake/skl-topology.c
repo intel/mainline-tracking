9a7bad4e3d96d77cf863cdad48e0afeabd16c8b7
Cezary Rojewski <cezary.rojewski@intel.com>
2018-09-03 14:42:21 +0200

    ASoC: Intel: Skylake: Fix hw_params updating for BE
    This change fixes regression introduced in 4.20-rc1 kernel base with DAI
    management refactor.
    
    Previously existing CODEC-CODEC dais bind routines have been merged into
    standard dais binders. Because of this, the amount of paths available
    for source and sink widgets has also changed.
    skl_tplg_be_set_src|sink_pipe_params cannot use the default -EIO return
    value since it is no longer guaranteed that any path contains at least
    one connected SKL widget.
    
    Change-Id: I7e41880d24f3c17ed33c823722cc0ad43b2a4ce2
    Signed-off-by: Cezary Rojewski <cezary.rojewski@intel.com>

--- sound/soc/intel/skylake/skl-topology.c
+++ sound/soc/intel/skylake/skl-topology.c
@@ -1775,7 +1775,7 @@ static int skl_tplg_be_set_src_pipe_params(struct snd_soc_dai *dai,
 				struct skl_pipe_params *params)
 {
 	struct snd_soc_dapm_path *p;
-	int ret = -EIO;
+	int ret;
 
 	snd_soc_dapm_widget_for_each_source_path(w, p) {
 		if (p->connect && is_skl_dsp_widget_type(p->source, dai->dev) &&
@@ -1793,14 +1793,14 @@ static int skl_tplg_be_set_src_pipe_params(struct snd_soc_dai *dai,
 		}
 	}
 
-	return ret;
+	return 0;
 }
 
 static int skl_tplg_be_set_sink_pipe_params(struct snd_soc_dai *dai,
 	struct snd_soc_dapm_widget *w, struct skl_pipe_params *params)
 {
 	struct snd_soc_dapm_path *p = NULL;
-	int ret = -EIO;
+	int ret;
 
 	snd_soc_dapm_widget_for_each_sink_path(w, p) {
 		if (p->connect && is_skl_dsp_widget_type(p->sink, dai->dev) &&
@@ -1818,7 +1818,7 @@ static int skl_tplg_be_set_sink_pipe_params(struct snd_soc_dai *dai,
 		}
 	}
 
-	return ret;
+	return 0;
 }
 
 /*
