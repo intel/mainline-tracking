65d7b1165e1b97013f069d21e3aad772dbc98fd6
Cezary Rojewski <cezary.rojewski@intel.com>
2019-03-22 19:34:24 +0100

    ASoC: Intel: Skylake: Unify firmware loading mechanism
    There are certain operations we want to do before and after firmware
    loading e.g.: disabling/ enabling power and clock gating. To make code
    coherent, provide skl_init_fw as a unified way for loading dsp firmware.
    
    In consequence, this change provides CNL load library support during fw
    initialization which was previously missing.
    
    skl_dsp_fw_ops already takes care of fw and library load customization.
    New post-load additions in form of fw and hw config assignments make
    this change even more welcome.
    
    Change-Id: I58f3a36ecf4558483073ea2995651f332bb122a4
    Signed-off-by: Cezary Rojewski <cezary.rojewski@intel.com>

--- sound/soc/intel/skylake/cnl-sst-dsp.h
+++ sound/soc/intel/skylake/cnl-sst-dsp.h
@@ -97,7 +97,6 @@ void cnl_ipc_free(struct sst_generic_ipc *ipc);
 int cnl_sst_dsp_init(struct device *dev, void __iomem *mmio_base, int irq,
 		     const char *fw_name, struct skl_dsp_loader_ops dsp_ops,
 		     struct skl_dev **dsp);
-int cnl_sst_init_fw(struct device *dev, struct skl_dev *skl);
 void cnl_sst_dsp_cleanup(struct device *dev, struct skl_dev *skl);
 
 #endif /*__CNL_SST_DSP_H__*/
