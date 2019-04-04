intel/linux-intel-lts

[WORKAROUND] ASoC: Intel: Skylake: fix for BXT HW data loss in 16/16 -> 32/32 copier
This commit fixes Broxton hardware bug: playback data loss when using
copier with input 16 bit sample size in 16 bit container, output 32 bit
sample size in 32 bit container.

Workaruond method:
clear PPCTL.PROCEN bit for corresponding stream index
before writing to Stream Descriptor Format register (SDxFMT)
and set PPCTL.PROCEN again after that.

Change-Id: Ib6dd97f94c7dfbe609a5712c5a47248646ac10ff
Tracked-On: ACI-5615
Tracked-On: OAM-75492
Signed-off-by: Paweł Harłoziński <pawel.harlozinski@intel.com>
Reviewed-by: Lewandowski, Gustaw <gustaw.lewandowski@intel.com>
Tested-by: Lewandowski, Gustaw <gustaw.lewandowski@intel.com>
Tested-by: gkblditp <gkblditp@intel.com>

sound/soc/intel/skylake/skl-pcm.c
@@ -38,6 +38,9 @@
 #define HDA_QUAD 4
 #define HDA_8_CH 8
 
+#define IS_BXT(pci) ((pci)->vendor == 0x8086 && \
+			((pci)->device == 0x5a98 || (pci)->device == 0x1a98))
+
 static struct snd_pcm_hardware azx_pcm_hw = {
 	.info =			(SNDRV_PCM_INFO_MMAP |
 				 SNDRV_PCM_INFO_INTERLEAVED |
@@ -186,6 +189,7 @@ int skl_pcm_host_dma_prepare(struct device *dev, struct skl_pipe_params *params)
 	struct hdac_ext_stream *stream;
 	struct snd_pcm_runtime *runtime;
 	int err;
+	struct skl *skl = ebus_to_skl(ebus);
 
 	hstream = snd_hdac_get_stream(bus, params->stream,
 					params->host_dma_id + 1);
@@ -206,7 +210,14 @@ int skl_pcm_host_dma_prepare(struct device *dev, struct skl_pipe_params *params)
 	if (err < 0)
 		return err;
 
+	if (IS_BXT(skl->pci))	/* workaround for BXT HW bug */
+		snd_hdac_ext_stream_decouple(ebus, stream, false);
+
 	err = snd_hdac_stream_setup(hdac_stream(stream));
+
+	if (IS_BXT(skl->pci))	/* workaround for BXT HW bug */
+		snd_hdac_ext_stream_decouple(ebus, stream, true);
+
 	if (err < 0)
 		return err;
 

intel/mainline-tracking

sound/soc/intel/skylake/skl-pcm.c
@@ -175,6 +175,7 @@ int skl_pcm_host_dma_prepare(struct device *dev, struct skl_pipe_params *params)
 	struct hdac_ext_stream *stream;
 	struct snd_pcm_runtime *runtime;
 	int err;
+	struct skl *skl = bus_to_skl(bus);
 
 	hstream = snd_hdac_get_stream(bus, params->stream,
 					params->host_dma_id + 1);
@@ -195,7 +196,14 @@ int skl_pcm_host_dma_prepare(struct device *dev, struct skl_pipe_params *params)
 	if (err < 0)
 		return err;
 
+	if (IS_BXT(skl->pci))	/* workaround for BXT HW bug */
+		snd_hdac_ext_stream_decouple(bus, stream, false);
+
 	err = snd_hdac_stream_setup(hdac_stream(stream));
+
+	if (IS_BXT(skl->pci))	/* workaround for BXT HW bug */
+		snd_hdac_ext_stream_decouple(bus, stream, true);
+
 	if (err < 0)
 		return err;
 