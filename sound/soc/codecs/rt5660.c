f62ecbd4b209298921ef1cb60ef4d25c6bcd69fe
Lai, Poey Seng <poey.seng.lai@intel.com>
2019-05-15 15:29:09 +0800

    [REVERTME]ASoC: Intel: boards: IP FPGA testing for Elkhart Lake rt5660 machine driver
    Add support for testing Elkhart Lake ALC5660 audio
    machine driver using IP FPGA with KBL RVP board.
    
    Change-Id: I77f684984bd9660213840546ecd65a01fb169686
    Signed-off-by: Lai, Poey Seng <poey.seng.lai@intel.com>
    Reviewed-on: 
    Tested-by: gkblditp <gkblditp@intel.com>
    Reviewed-by: Lewandowski, Gustaw <gustaw.lewandowski@intel.com>

--- sound/soc/codecs/rt5660.c
+++ sound/soc/codecs/rt5660.c
@@ -1245,6 +1245,7 @@ static const struct acpi_device_id rt5660_acpi_match[] = {
 	{ "10EC5660", 0 },
 	{ "10EC3277", 0 },
 	{ "INTC1027", 0 },
+	{ "INT34C2", 0 },
 	{ },
 };
 MODULE_DEVICE_TABLE(acpi, rt5660_acpi_match);
