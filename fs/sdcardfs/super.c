fb213b4660829a3f7d3c88cc598644e15891ca81
Kris Pan <kris.pan@intel.com>
2019-06-12 11:19:00 +0800

    Android: sdcardfs: align sdcardfs with v4.19
    Signed-off-by: Kris Pan

--- fs/sdcardfs/super.c
+++ fs/sdcardfs/super.c
@@ -147,6 +147,7 @@ static int sdcardfs_remount_fs2(struct vfsmount *mnt, struct super_block *sb,
 	pr_info("Remount options were %s for vfsmnt %p.\n", options, mnt);
 	err = parse_options_remount(sb, options, *flags & ~MS_SILENT, mnt->data);
 
+
 	return err;
 }
 
@@ -310,6 +311,8 @@ static int sdcardfs_show_options(struct vfsmount *mnt, struct seq_file *m,
 		seq_puts(m, ",default_normal");
 	if (opts->reserved_mb != 0)
 		seq_printf(m, ",reserved=%uMB", opts->reserved_mb);
+	if (opts->nocache)
+		seq_printf(m, ",nocache");
 
 	return 0;
 };
