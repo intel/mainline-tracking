186ab6ffbf2430cd58e5c9a63f3303103aca5da9
Wong, Vincent Por Yin <vincent.por.yin.wong@intel.com>
2019-08-09 22:17:52 +0800

    net: stmmac: Zero-copy transmit and XDP_TX support
    - AF_XDP ZC support, uses the same TX queue for packet queueing
    - support for AF_XDP ZC Receive path's XDP_TX
    
    Signed-off-by: Wong, Vincent Por Yin <vincent.por.yin.wong@intel.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_xsk.h
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_xsk.h
@@ -8,6 +8,7 @@
 #define STMMAC_XDP_DROP		BIT(0)
 #define STMMAC_XDP_TX		BIT(1)
 #define STMMAC_XDP_REDIRECT	BIT(2)
+#define STMMAC_DEFAULT_TX_WORK	256
 
 #define queue_is_xdp(qid) (priv->is_xdp[(qid)])
 #define enable_queue_xdp(qid) { priv->is_xdp[(qid)] = true; }
@@ -36,4 +37,8 @@ void stmmac_reuse_rx_buffer_zc(struct stmmac_rx_queue *rx_q,
 			       struct stmmac_rx_buffer *obi);
 int stmmac_rx_zc(struct stmmac_priv *priv, const int budget, u32 qid);
 void stmmac_xsk_free_rx_ring(struct stmmac_rx_queue *rx_q);
+void stmmac_xsk_free_tx_ring(struct stmmac_tx_queue *tx_q);
+int stmmac_xsk_async_xmit(struct net_device *dev, u32 qid);
+bool stmmac_xdp_xmit_zc(struct stmmac_tx_queue *tx_q, unsigned int budget);
+
 #endif /* __STMMAC_XSK_H__ */
