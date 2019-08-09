51024093bffcfea9199f109da0afe26ea351a91c
Mateusz Gorski <mateusz.gorski@intel.com>
2019-08-09 10:44:20 +0200

    ASoC: Intel: Skylake: Workaround for SPA for TGL/EHL platforms
    For cAVS 2.5 platforms we are no longer able to control powering up/down
    DSP cores higher that core0, it is now done in firmware. Workaround patch
    to adjust our power flow for these platforms.
    
    Change-Id: I06d1fd47c84c2fb648357f6400036fa7e7fb079f
    Signed-off-by: Mateusz Gorski <mateusz.gorski@intel.com>
    Reviewed-on: 
    Reviewed-by: Slawinski, AmadeuszX <amadeuszx.slawinski@intel.com>
    Tested-by: gkblditp <gkblditp@intel.com>
    Reviewed-by: Rojewski, Cezary <cezary.rojewski@intel.com>

--- sound/soc/intel/skylake/cnl-sst-dsp.c
+++ sound/soc/intel/skylake/cnl-sst-dsp.c
@@ -17,6 +17,7 @@
 #include "../common/sst-dsp-priv.h"
 #include "cnl-sst-dsp.h"
 #include "skl.h"
+#include <linux/pci.h>
 
 /* various timeout values */
 #define CNL_DSP_PU_TO		50
@@ -141,6 +142,11 @@ static int cnl_dsp_core_power_down(struct sst_dsp *ctx, unsigned int core_mask)
 int cnl_dsp_enable_core(struct sst_dsp *ctx, unsigned int core_mask)
 {
 	int ret;
+	struct skl_dev *skl = ctx->thread_context;
+
+	if ((IS_EHL(skl->pci) || IS_TGL(skl->pci)) &&
+			core_mask != SKL_DSP_CORE0_MASK)
+		return 0;
 
 	/* power up */
 	ret = cnl_dsp_core_power_up(ctx, core_mask);
@@ -156,6 +162,11 @@ int cnl_dsp_enable_core(struct sst_dsp *ctx, unsigned int core_mask)
 int cnl_dsp_disable_core(struct sst_dsp *ctx, unsigned int core_mask)
 {
 	int ret;
+	struct skl_dev *skl = ctx->thread_context;
+
+	if ((IS_EHL(skl->pci) || IS_TGL(skl->pci)) &&
+			core_mask != SKL_DSP_CORE0_MASK)
+		return 0;
 
 	ret = cnl_dsp_reset_core(ctx, core_mask);
 	if (ret < 0) {
