4d711e7567fef1926fbc389df14efc2d73002efb
Yu-cheng Yu <yu-cheng.yu@intel.com>
2019-08-22 10:06:11 -0700

    x86/cet/shstk: User-mode shadow stack support
    This patch adds basic shadow stack enabling/disabling routines.  A task's
    shadow stack is allocated from memory with VM_SHSTK flag and has a fixed
    size of min(RLIMIT_STACK, 4GB).
    
    Signed-off-by: Yu-cheng Yu <yu-cheng.yu@intel.com>
    
    v12:
    - Split cet_disable_free_shstk() into disable and free parts, because
      only the disable shadow stack arch_prctl() needs the disable part.
      Other cases need only the free part.  In an exit syscall, there is
      no need to write CET MSRs.
    
    v11:
    - Modify alloc_shstk() to take flags and pass to do_mmap().
      This is to be used by an arch_prctl() introduced later.
    
    v10:
    - Change no_cet_shstk to no_user_shstk.
    - Limit shadow stack size to 4 GB, and round_up to PAGE_SIZE.
    - Replace checking shstk_enabled with shstk_size being zero.
    - WARN_ON_ONCE() when vm_munmap() fails.
    
    v9:
    - Change cpu_feature_enabled() to static_cpu_has().
    - Merge cet_disable_shstk to cet_disable_free_shstk.
    - Remove the empty slot at the top of the shadow stack, as it is not
      needed.
    - Move do_mmap_locked() to alloc_shstk(), which is a static function.
    
    v6:
    - Create a function do_mmap_locked() for shadow stack allocation.
    
    v2:
    - Change noshstk to no_cet_shstk.

--- arch/x86/include/asm/processor.h
+++ arch/x86/include/asm/processor.h
@@ -27,6 +27,7 @@ struct vm86;
 #include <asm/unwind_hints.h>
 #include <asm/vmxfeatures.h>
 #include <asm/vdso/processor.h>
+#include <asm/cet.h>
 
 #include <linux/personality.h>
 #include <linux/cache.h>
@@ -541,6 +542,10 @@ struct thread_struct {
 	mm_segment_t		addr_limit;
 
 	unsigned int		sig_on_uaccess_err:1;
+
+#ifdef CONFIG_X86_CET
+	struct cet_status	cet;
+#endif
 
 	/* Floating point and extended processor state */
 	struct fpu		fpu;
