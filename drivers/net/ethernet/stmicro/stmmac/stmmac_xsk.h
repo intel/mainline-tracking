3639b1efe84b364e4888158c888d7e76d466e205
Wong, Vincent Por Yin <vincent.por.yin.wong@intel.com>
2019-08-27 16:13:11 +0800

    net: stmmac: add defines and helper function for AF_XDP
    Added defines for tracking XDP return codes and a helper function
    stmmac_enabled_xdp() to check if an XDP program is loaded.
    
    Signed-off-by: Wong, Vincent Por Yin <vincent.por.yin.wong@intel.com>

--- drivers/net/ethernet/stmicro/stmmac/stmmac_xsk.h
+++ drivers/net/ethernet/stmicro/stmmac/stmmac_xsk.h
@@ -4,8 +4,18 @@
 #ifndef __STMMAC_XSK_H__
 #define __STMMAC_XSK_H__
 
+#define STMMAC_XDP_PASS		0
+#define STMMAC_XDP_DROP		BIT(0)
+#define STMMAC_XDP_TX		BIT(1)
+#define STMMAC_XDP_REDIRECT	BIT(2)
+
 #define queue_is_xdp(qid) (priv->is_xdp[(qid)])
 #define enable_queue_xdp(qid) { priv->is_xdp[(qid)] = true; }
 #define disable_queue_xdp(qid) { priv->is_xdp[(qid)] = false; }
+
+static inline bool stmmac_enabled_xdp(struct stmmac_priv *priv)
+{
+	return !!priv->xdp_prog;
+}
 
 #endif /* __STMMAC_XSK_H__ */
