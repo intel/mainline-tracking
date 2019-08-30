39a4fad51af2c5cd1877f54f71057270681184d0
Amadeusz Sławiński <amadeuszx.slawinski@linux.intel.com>
2019-08-30 13:19:59 +0200

    ASoC: Intel: bxt_rt298: Remove dmic16k from glk
    Actually there is no such thing as DMIC16k, it's the same port as DMIC.
    This was probably done for some kind of testing setup, but I don't see
    how this makes sense when we can just use DMIC.
    
    Change-Id: I77208c6259a5941a69686d2f4f4acfdd05640e8b
    Signed-off-by: Amadeusz Sławiński <amadeuszx.slawinski@linux.intel.com>
    Reviewed-on: 
    Tested-by: gkblditp <gkblditp@intel.com>
    Reviewed-by: Rojewski, Cezary <cezary.rojewski@intel.com>

--- sound/soc/intel/boards/bxt_rt298.c
+++ sound/soc/intel/boards/bxt_rt298.c
@@ -139,9 +139,6 @@ static const struct snd_soc_dapm_route geminilake_rt298_map[] = {
 	{ "dmic01_hifi", NULL, "DMIC01 Rx" },
 	{ "DMIC01 Rx", NULL, "Capture" },
 
-	{ "dmic_voice", NULL, "DMIC16k Rx" },
-	{ "DMIC16k Rx", NULL, "Capture" },
-
 	{ "hifi3", NULL, "iDisp3 Tx"},
 	{ "iDisp3 Tx", NULL, "iDisp3_out"},
 	{ "hifi2", NULL, "iDisp2 Tx"},
@@ -357,9 +354,6 @@ SND_SOC_DAILINK_DEF(dmic_codec,
 	DAILINK_COMP_ARRAY(COMP_CODEC("dmic-codec",
 				      "dmic-hifi")));
 
-SND_SOC_DAILINK_DEF(dmic16k,
-	DAILINK_COMP_ARRAY(COMP_CPU("DMIC16k Pin")));
-
 SND_SOC_DAILINK_DEF(idisp1_pin,
 	DAILINK_COMP_ARRAY(COMP_CPU("iDisp1 Pin")));
 SND_SOC_DAILINK_DEF(idisp1_codec,
@@ -489,17 +483,8 @@ static struct snd_soc_dai_link broxton_rt298_dais[] = {
 		SND_SOC_DAILINK_REG(dmic_pin, dmic_codec, platform),
 	},
 	{
-		.name = "dmic16k",
-		.id = 2,
-		.be_hw_params_fixup = broxton_dmic_fixup,
-		.ignore_suspend = 1,
-		.dpcm_capture = 1,
-		.no_pcm = 1,
-		SND_SOC_DAILINK_REG(dmic16k, dmic_codec, platform),
-	},
-	{
 		.name = "iDisp1",
-		.id = 3,
+		.id = 2,
 		.init = broxton_hdmi_init,
 		.dpcm_playback = 1,
 		.no_pcm = 1,
@@ -507,7 +492,7 @@ static struct snd_soc_dai_link broxton_rt298_dais[] = {
 	},
 	{
 		.name = "iDisp2",
-		.id = 4,
+		.id = 3,
 		.init = broxton_hdmi_init,
 		.dpcm_playback = 1,
 		.no_pcm = 1,
@@ -515,7 +500,7 @@ static struct snd_soc_dai_link broxton_rt298_dais[] = {
 	},
 	{
 		.name = "iDisp3",
-		.id = 5,
+		.id = 4,
 		.init = broxton_hdmi_init,
 		.dpcm_playback = 1,
 		.no_pcm = 1,
