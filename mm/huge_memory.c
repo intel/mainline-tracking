5ee799507f28f25e904afa6171850a8cc5944230
Yu-cheng Yu <yu-cheng.yu@intel.com>
2020-04-07 16:03:36 -0700

    x86/mm: Update maybe_mkwrite() for shadow stack
    When serving a page fault, maybe_mkwrite() makes a PTE writable if its vma
    has VM_WRITE.
    
    A shadow stack vma has VM_SHSTK.  Its PTEs have _PAGE_DIRTY, but not
    _PAGE_WRITE.  In fork(), _PAGE_DIRTY is cleared to effect copy-on-write,
    and in page fault, _PAGE_DIRTY is restored and the shadow stack page is
    writable again.
    
    Update maybe_mkwrite() by introducing arch_maybe_mkwrite(), which sets
    _PAGE_DIRTY for a shadow stack PTE.
    
    Apply the same changes to maybe_pmd_mkwrite().
    
    Signed-off-by: Yu-cheng Yu <yu-cheng.yu@intel.com>
    Reviewed-by: Kees Cook <keescook@chromium.org>

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -478,6 +478,8 @@ pmd_t maybe_pmd_mkwrite(pmd_t pmd, struct vm_area_struct *vma)
 {
 	if (likely(vma->vm_flags & VM_WRITE))
 		pmd = pmd_mkwrite(pmd);
+	else
+		pmd = arch_maybe_pmd_mkwrite(pmd, vma);
 	return pmd;
 }
 
