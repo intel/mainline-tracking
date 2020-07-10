f1287a0988bd152c3b628f7316897897f39b89d0
Ira Weiny <ira.weiny@intel.com>
2020-07-09 23:18:12 -0700

    x86/pks: Preserve the PKRS MSR on context switch
    The PKRS MSR is defined as a per-logical-processor register.  This
    isolates memory access by logical CPU.  Unfortunately, the MSR is not
    managed by XSAVE.  Therefore, tasks must save/restore the MSR value on
    context switch.
    
    Define a saved PKRS value in the task struct, as well as a cached
    per-logical-processor MSR value which mirrors the MSR value of the
    current CPU.  Initialize all tasks with the default MSR value.  Then, on
    schedule in, call write_pkrs() which automatically avoids the overhead
    of the MSR write if possible.
    
    Co-developed-by: Fenghua Yu <fenghua.yu@intel.com>
    Signed-off-by: Fenghua Yu <fenghua.yu@intel.com>
    Signed-off-by: Ira Weiny <ira.weiny@intel.com>

--- arch/x86/kernel/process_64.c
+++ arch/x86/kernel/process_64.c
@@ -632,6 +632,8 @@ __switch_to(struct task_struct *prev_p, struct task_struct *next_p)
 	/* Load the Intel cache allocation PQR MSR. */
 	resctrl_sched_in();
 
+	pks_sched_in();
+
 	return prev_p;
 }
 
