ce46ede0ff75e2aff25d3df843c9953bede9fd32
Patrik Flykt <patrik.flykt@linux.intel.com>
2020-08-25 13:17:36 +0300

    NET: mcan: Move runtime PM enable/disable to m_can_platform
    This is a preparatory patch for upcoming PCI based M_CAN devices.
    The current PM implementation would cause PCI based drivers to
    enable PM twice, once when the pci device is added and a second
    time in m_can_class_register(). This will cause 'Unbalanced
    pm_runtime_enable!' to be logged, and is a situation that should
    be avoided.
    
    Therefore, in anticipation of PCI devices, move PM enabling out
    from M_CAN class registration to its currently only user, the
    m_can_platform driver.
    
    Signed-off-by: Patrik Flykt <patrik.flykt@linux.intel.com>
    Signed-off-by: Jarkko Nikula <jarkko.nikula@linux.intel.com>

--- drivers/net/can/m_can/m_can.c
+++ drivers/net/can/m_can/m_can.c
@@ -1817,7 +1817,6 @@ int m_can_class_register(struct m_can_classdev *m_can_dev)
 	int ret;
 
 	if (m_can_dev->pm_clock_support) {
-		pm_runtime_enable(m_can_dev->dev);
 		ret = m_can_clk_start(m_can_dev);
 		if (ret)
 			goto pm_runtime_fail;
@@ -1847,11 +1846,8 @@ int m_can_class_register(struct m_can_classdev *m_can_dev)
 clk_disable:
 	m_can_clk_stop(m_can_dev);
 pm_runtime_fail:
-	if (ret) {
-		if (m_can_dev->pm_clock_support)
-			pm_runtime_disable(m_can_dev->dev);
+	if (ret)
 		free_candev(m_can_dev->net);
-	}
 
 	return ret;
 }
