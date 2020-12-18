dbc3aec961bdaeefda74337f2097091d0332d9b8
Ankit Nautiyal <ankit.k.nautiyal@intel.com>
2020-12-18 16:07:17 +0530

    drm/i915: Add support for starting FRL training for HDMI2.1 via PCON
    This patch adds functions to start FRL training for an HDMI2.1 sink,
    connected via a PCON as a DP branch device.
    This patch also adds a new structure for storing frl training related
    data, when FRL training is completed.
    
    v2: As suggested by Uma Shankar:
    -renamed couple of variables for better clarity
    -tweaked the macros used for correct semantics for true/false
    -fixed other styling issues.
    
    v3: Completed the TODO for condition for going to FRL mode.
    Modified the condition to determine the required FRL b/w
    based only on the Pcon and Sink's max FRL values.
    Moved the frl structure initialization to intel_dp_init_connector().
    
    v4: Fixed typo in initialization of frl structure.
    
    v5: Always use FRL if its possible, instead of enabling only for
    higher modes as done in v3.
    
    Signed-off-by: Ankit Nautiyal <ankit.k.nautiyal@intel.com>
    Reviewed-by: Uma Shankar <uma.shankar@intel.com> (v2)
    [Jani: Fixed checkpatch BRACES, CONSTANT_COMPARISON.]
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20201218103723.30844-10-ankit.k.nautiyal@intel.com

--- drivers/gpu/drm/i915/display/intel_display_types.h
+++ drivers/gpu/drm/i915/display/intel_display_types.h
@@ -1339,6 +1339,11 @@ struct intel_dp_compliance {
 	u8 test_lane_count;
 };
 
+struct intel_dp_pcon_frl {
+	bool is_trained;
+	int trained_rate_gbps;
+};
+
 struct intel_dp {
 	i915_reg_t output_reg;
 	u32 DP;
@@ -1464,6 +1469,8 @@ struct intel_dp {
 
 	bool hobl_failed;
 	bool hobl_active;
+
+	struct intel_dp_pcon_frl frl;
 };
 
 enum lspcon_vendor {
