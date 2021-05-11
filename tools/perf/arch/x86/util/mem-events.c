a84c6c23536bce380c9bebec22c15c203aaf9231
Jin Yao <yao.jin@linux.intel.com>
2021-05-11 14:43:41 +0800

    perf mem: Support for hybrid platform
    On alderlake, the 'mem-loads' event is required to use together with
    'mem-loads-aux' within a group and 'mem-loads-aux' must be the group
    leader. Before reporting, we disable the grouping because 'mem-loads-aux'
    is just an auxiliary event. It actually doesn't carry valid load latency
    information. If we show the 'mem-loads-aux' + 'mem-loads' as a group in
    perf report, it needs many of changes but they are totally unnecessary.
    
    If we execute 'perf mem record', it actually calls:
    record -e {cpu_core/mem-loads-aux/,cpu_core/mem-loads,ldlat=30/}:P
           -e cpu_atom/mem-loads,ldlat=30/P
           -e cpu_core/mem-stores/P
           -e cpu_atom/mem-stores/P
    
    Signed-off-by: Jin Yao <yao.jin@linux.intel.com>

--- tools/perf/arch/x86/util/mem-events.c
+++ tools/perf/arch/x86/util/mem-events.c
@@ -4,10 +4,10 @@
 #include "mem-events.h"
 
 static char mem_loads_name[100];
-static bool mem_loads_name__init;
+static char mem_stores_name[100];
 
 #define MEM_LOADS_AUX		0x8203
-#define MEM_LOADS_AUX_NAME	"{cpu/mem-loads-aux/,cpu/mem-loads,ldlat=%u/pp}:S"
+#define MEM_LOADS_AUX_NAME     "{%s/mem-loads-aux/,%s/mem-loads,ldlat=%u/}:P"
 
 bool is_mem_loads_aux_event(struct evsel *leader)
 {
@@ -22,27 +22,33 @@ bool is_mem_loads_aux_event(struct evsel *leader)
 	return leader->core.attr.config == MEM_LOADS_AUX;
 }
 
-char *perf_mem_events__name(int i)
+char *perf_mem_events__name(int i, char *pmu_name)
 {
 	struct perf_mem_event *e = perf_mem_events__ptr(i);
 
 	if (!e)
 		return NULL;
 
+	if (!pmu_name)
+		pmu_name = (char *)"cpu";
+
 	if (i == PERF_MEM_EVENTS__LOAD) {
-		if (mem_loads_name__init)
-			return mem_loads_name;
-
-		mem_loads_name__init = true;
-
-		if (pmu_have_event("cpu", "mem-loads-aux")) {
+		if (pmu_have_event(pmu_name, "mem-loads-aux")) {
 			scnprintf(mem_loads_name, sizeof(mem_loads_name),
-				  MEM_LOADS_AUX_NAME, perf_mem_events__loads_ldlat);
-		} else {
-			scnprintf(mem_loads_name, sizeof(mem_loads_name),
-				  e->name, perf_mem_events__loads_ldlat);
+				  MEM_LOADS_AUX_NAME, pmu_name, pmu_name,
+				  perf_mem_events__loads_ldlat);
+                } else {
+                        scnprintf(mem_loads_name, sizeof(mem_loads_name),
+				  e->name, pmu_name,
+				  perf_mem_events__loads_ldlat);
 		}
 		return mem_loads_name;
+	}
+
+	if (i == PERF_MEM_EVENTS__STORE) {
+		scnprintf(mem_stores_name, sizeof(mem_stores_name),
+			  e->name, pmu_name);
+		return mem_stores_name;
 	}
 
 	return (char *)e->name;
