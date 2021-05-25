81af9a11d1e8dd94e153331c22289417d9409ae7
Sohil Mehta <sohil.mehta@intel.com>
2021-05-25 16:57:39 -0700

    x86/process/64: Add uintr task context switch support
    User interrupt state is saved and restored using xstate supervisor
    feature support. This includes the MSR state and the User Interrupt Flag
    (UIF) value.
    
    During context switch update the UPID for a uintr task to reflect the
    current state of the task; namely whether the task should receive
    interrupt notifications and which cpu the task is currently running on.
    
    XSAVES clears the notification vector (UINV) in the MISC MSR to prevent
    interrupts from being recognized in the UIRR MSR while the task is being
    context switched. The UINV is restored back when the kernel does an
    XRSTORS.
    
    However, this conflicts with the kernel's lazy restore optimization
    which skips an XRSTORS if the kernel is scheduling the same user task
    back and the underlying MSR state hasn't been modified. Special handling
    is needed for a uintr task in the context switch path to keep using this
    optimization.
    
    Signed-off-by: Jacob Pan <jacob.jun.pan@linux.intel.com>
    Signed-off-by: Sohil Mehta <sohil.mehta@intel.com>

--- arch/x86/include/asm/uintr.h
+++ arch/x86/include/asm/uintr.h
@@ -8,6 +8,15 @@ bool uintr_arch_enabled(void);
 int do_uintr_register_handler(u64 handler);
 int do_uintr_unregister_handler(void);
 
+/* TODO: Inline the context switch related functions */
+void switch_uintr_prepare(struct task_struct *prev);
+void switch_uintr_return(void);
+
+#else /* !CONFIG_X86_USER_INTERRUPTS */
+
+static inline void switch_uintr_prepare(struct task_struct *prev) {}
+static inline void switch_uintr_return(void) {}
+
 #endif /* CONFIG_X86_USER_INTERRUPTS */
 
 #endif /* _ASM_X86_UINTR_H */
