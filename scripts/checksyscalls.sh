fab448b99f1f81170000f3550fd047f3e0c75e0f
Sohil Mehta <sohil.mehta@intel.com>
2021-05-25 16:57:40 -0700

    x86/uintr: Wire up the user interrupt syscalls
    Wire up the user interrupt receiver and sender related syscalls for
    x86_64.
    
    For rest of the architectures the syscalls are not implemented.
    
    <TODO: Reserve the syscall numbers for other architectures>
    
    Signed-off-by: Sohil Mehta <sohil.mehta@intel.com>

--- scripts/checksyscalls.sh
+++ scripts/checksyscalls.sh
@@ -204,6 +204,12 @@ cat << EOF
 #define __IGNORE__sysctl
 #define __IGNORE_arch_prctl
 #define __IGNORE_nfsservctl
+#define __IGNORE_uintr_register_handler
+#define __IGNORE_uintr_unregister_handler
+#define __IGNORE_uintr_create_fd
+#define __IGNORE_uintr_register_sender
+#define __IGNORE_uintr_unregister_sender
+#define __IGNORE_uintr_wait
 
 /* ... including the "new" 32-bit uid syscalls */
 #define __IGNORE_lchown32
