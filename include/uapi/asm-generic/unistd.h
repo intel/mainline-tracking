74595d2a4f33ab2ca7c47677613b7ff4a01ca593
Sohil Mehta <sohil.mehta@intel.com>
2021-05-25 16:57:40 -0700

    x86/uintr: Wire up the user interrupt syscalls
    Wire up the user interrupt receiver and sender related syscalls for
    x86_64.
    
    For rest of the architectures the syscalls are not implemented.
    
    <TODO: Reserve the syscall numbers for other architectures>
    
    Signed-off-by: Sohil Mehta <sohil.mehta@intel.com>

--- include/uapi/asm-generic/unistd.h
+++ include/uapi/asm-generic/unistd.h
@@ -883,8 +883,21 @@ __SYSCALL(__NR_process_mrelease, sys_process_mrelease)
 #define __NR_futex_waitv 449
 __SYSCALL(__NR_futex_waitv, sys_futex_waitv)
 
+#define __NR_uintr_register_handler 450
+__SYSCALL(__NR_uintr_register_handler, sys_uintr_register_handler)
+#define __NR_uintr_unregister_handler 451
+__SYSCALL(__NR_uintr_unregister_handler, sys_uintr_unregister_handler)
+#define __NR_uintr_create_fd 452
+__SYSCALL(__NR_uintr_create_fd, sys_uintr_create_fd)
+#define __NR_uintr_register_sender 453
+__SYSCALL(__NR_uintr_register_sender, sys_uintr_register_sender)
+#define __NR_uintr_unregister_sender 454
+__SYSCALL(__NR_uintr_unregister_sender, sys_uintr_unregister_sender)
+#define __NR_uintr_wait 455
+__SYSCALL(__NR_uintr_wait, sys_uintr_wait)
+
 #undef __NR_syscalls
-#define __NR_syscalls 450
+#define __NR_syscalls 456
 
 /*
  * 32 bit systems traditionally used different
