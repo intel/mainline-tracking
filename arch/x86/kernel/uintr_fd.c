a8a9347d160acad29f8ca9c9ec8015c6289a7312
Sohil Mehta <sohil.mehta@intel.com>
2021-05-25 16:57:39 -0700

    x86/uintr: Introduce vector registration and uintr_fd syscall
    Each receiving task has its own interrupt vector space of 64 vectors.
    For each vector registered by a task create a uintr_fd. Only tasks that
    have previously registered a user interrupt handler can register a
    vector.
    
    The sender for the user interrupt could be another userspace
    application, kernel or an external source (like a device). Any sender
    that wants to generate a user interrupt needs access to receiver's
    vector number and UPID.  uintr_fd abstracts that information and allows
    a sender with access to uintr_fd to connect and generate a user
    interrupt. Upon interrupt delivery, the interrupt handler would be
    invoked with the associated vector number pushed onto the stack.
    
    Using an FD abstraction automatically provides a secure mechanism to
    connect with a receiver. It also makes the tracking and management of
    the interrupt vector resource easier for userspace.
    
    uintr_fd can be useful in some of the usages where eventfd is used for
    userspace event notifications. Though uintr_fd is nowhere close to a
    drop-in replacement, the semantics are meant to be somewhat similar to
    an eventfd or the write end of a pipe.
    
    Access to uintr_fd can be achieved in the following ways:
    - direct access if the task is part of the same thread group (process)
    - inherited by a child process.
    - explicitly shared using any of the FD sharing mechanisms.
    
    If the sender is another userspace task, it can use the uintr_fd to send
    user IPIs to the receiver. This works in conjunction with the SENDUIPI
    instruction. The details related to this are covered later.
    
    The exact APIs for the sender being a kernel or another external source
    are still being worked upon. The general idea is that the receiver would
    pass the uintr_fd to the kernel by extending some existing API (like
    io_uring).
    
    The vector associated with uintr_fd can be unregistered by closing all
    references to the uintr_fd.
    
    Signed-off-by: Sohil Mehta <sohil.mehta@intel.com>

--- arch/x86/kernel/uintr_fd.c
+++ arch/x86/kernel/uintr_fd.c
@@ -6,10 +6,104 @@
  */
 #define pr_fmt(fmt)	"uintr: " fmt
 
+#include <linux/anon_inodes.h>
+#include <linux/fdtable.h>
 #include <linux/sched.h>
 #include <linux/syscalls.h>
 
 #include <asm/uintr.h>
+
+struct uintrfd_ctx {
+	struct uintr_receiver_info *r_info;
+};
+
+#ifdef CONFIG_PROC_FS
+static void uintrfd_show_fdinfo(struct seq_file *m, struct file *file)
+{
+	struct uintrfd_ctx *uintrfd_ctx = file->private_data;
+
+	/* Check: Should we print the receiver and sender info here? */
+	seq_printf(m, "user_vector:%llu\n", uintrfd_ctx->r_info->uvec);
+}
+#endif
+
+static int uintrfd_release(struct inode *inode, struct file *file)
+{
+	struct uintrfd_ctx *uintrfd_ctx = file->private_data;
+
+	pr_debug("recv: Release uintrfd for r_task %d uvec %llu\n",
+		 uintrfd_ctx->r_info->upid_ctx->task->pid,
+		 uintrfd_ctx->r_info->uvec);
+
+	do_uintr_unregister_vector(uintrfd_ctx->r_info);
+	kfree(uintrfd_ctx);
+
+	return 0;
+}
+
+static const struct file_operations uintrfd_fops = {
+#ifdef CONFIG_PROC_FS
+	.show_fdinfo	= uintrfd_show_fdinfo,
+#endif
+	.release	= uintrfd_release,
+	.llseek		= noop_llseek,
+};
+
+/*
+ * sys_uintr_create_fd - Create a uintr_fd for the registered interrupt vector.
+ */
+SYSCALL_DEFINE2(uintr_create_fd, u64, vector, unsigned int, flags)
+{
+	struct uintrfd_ctx *uintrfd_ctx;
+	int uintrfd;
+	int ret;
+
+	if (!uintr_arch_enabled())
+		return -EOPNOTSUPP;
+
+	if (flags)
+		return -EINVAL;
+
+	uintrfd_ctx = kzalloc(sizeof(*uintrfd_ctx), GFP_KERNEL);
+	if (!uintrfd_ctx)
+		return -ENOMEM;
+
+	uintrfd_ctx->r_info = kzalloc(sizeof(*uintrfd_ctx->r_info), GFP_KERNEL);
+	if (!uintrfd_ctx->r_info) {
+		ret = -ENOMEM;
+		goto out_free_ctx;
+	}
+
+	uintrfd_ctx->r_info->uvec = vector;
+	ret = do_uintr_register_vector(uintrfd_ctx->r_info);
+	if (ret) {
+		kfree(uintrfd_ctx->r_info);
+		goto out_free_ctx;
+	}
+
+	/* TODO: Get user input for flags - UFD_CLOEXEC */
+	/* Check: Do we need O_NONBLOCK? */
+	uintrfd = anon_inode_getfd("[uintrfd]", &uintrfd_fops, uintrfd_ctx,
+				   O_RDONLY | O_CLOEXEC | O_NONBLOCK);
+
+	if (uintrfd < 0) {
+		ret = uintrfd;
+		goto out_free_uvec;
+	}
+
+	pr_debug("recv: Alloc vector success uintrfd %d uvec %llu for task=%d\n",
+		 uintrfd, uintrfd_ctx->r_info->uvec, current->pid);
+
+	return uintrfd;
+
+out_free_uvec:
+	do_uintr_unregister_vector(uintrfd_ctx->r_info);
+out_free_ctx:
+	kfree(uintrfd_ctx);
+	pr_debug("recv: Alloc vector failed for task=%d ret %d\n",
+		 current->pid, ret);
+	return ret;
+}
 
 /*
  * sys_uintr_register_handler - setup user interrupt handler for receiver.
