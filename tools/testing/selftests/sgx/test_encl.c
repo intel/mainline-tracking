bd66f55d2331bfd82d5a67a5aa190ccd4de9ede9
Reinette Chatre <reinette.chatre@intel.com>
2021-06-07 14:41:53 -0700

    selftests/sgx: Add test for EPCM permission changes
    EPCM permission changes could be made from within (to extend
    permissions) or out (to restrict permissions) the enclave. OS
    support is needed when permissions are restricted to be able to
    call the privileged ENCLS[EMODPR] instruction and ensure PTEs
    allowing the restricted permissions are flushed. EPCM permissions
    can be extended via ENCLU[EMODPE] from within the enclave.
    
    Add a test that exercises a few of the enclave page permission flows:
    1) Test starts with a RW (from enclave and OS perspective) enclave page
       that is mapped via a RW VMA.
    2) The SGX_IOC_PAGE_MODP ioctl is used to restrict the enclave (EPCM)
       page permissions to read-only (OS removes page table entry in the
       process).
    3) Run ENCLU[EACCEPT] from within the enclave to accept the new page
       permissions.
    4) Attempt to write to the enclave page from within the enclave - this
       should fail with a page fault on the page table entry since the page
       table entry accurately reflects the EPCM permissions.
    5) Restore EPCM permissions to RW by running ENCLU[EMODPE] from within
       the enclave.
    6) Attempt to write to the enclave page from within the enclave - this
       should fail again with a page fault because even though the EPCM
       permissions are RW the page table entries do not yet reflect that.
    7) The SGX_IOC_PAGE_MODP ioctl is used to inform OS of new page
       permissions and page table entries will accurately reflect RW EPCM
       permissions.
    8) Writing to enclave page from within enclave succeeds.
    9) Ensure EPCM.PR is clear by running ENCLU[EACCEPT].
    
    Signed-off-by: Reinette Chatre <reinette.chatre@intel.com>

--- tools/testing/selftests/sgx/test_encl.c
+++ tools/testing/selftests/sgx/test_encl.c
@@ -6,6 +6,42 @@
 
 static uint8_t encl_buffer[8192] = { 1 };
 
+enum sgx_enclu_function {
+	EACCEPT = 0x5,
+	EMODPE = 0x6,
+};
+
+static void do_encl_emodpe(void *_op)
+{
+	struct sgx_secinfo secinfo __aligned(sizeof(struct sgx_secinfo)) = {0};
+	struct encl_op_emodpe *op = _op;
+
+	secinfo.flags = op->flags;
+
+	asm volatile(".byte 0x0f, 0x01, 0xd7"
+				:
+				: "a" (EMODPE),
+				  "b" (&secinfo),
+				  "c" (op->epc_addr));
+}
+
+static void do_encl_eaccept(void *_op)
+{
+	struct sgx_secinfo secinfo __aligned(sizeof(struct sgx_secinfo)) = {0};
+	struct encl_op_eaccept *op = _op;
+	int rax;
+
+	secinfo.flags = op->flags;
+
+	asm volatile(".byte 0x0f, 0x01, 0xd7"
+				: "=a" (rax)
+				: "a" (EACCEPT),
+				  "b" (&secinfo),
+				  "c" (op->epc_addr));
+
+	op->ret = rax;
+}
+
 static void *memcpy(void *dest, const void *src, size_t n)
 {
 	size_t i;
