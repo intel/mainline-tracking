eb85f528e35ab8ba80e24270dfa4935ef4900fb0
Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>
2021-06-07 21:43:09 -0700

    x86/tdx: Handle port I/O in decompression code
    Add support to replace in/out instructions in decompression code with
    TDX IO hypercalls.
    
    TDX cannot do port IO directly. The TDX module triggers a #VE exception
    to let the guest kernel to emulate port I/O, by converting them into
    TDX hypercalls to call the host.
    
    But for the really early code in the decompressor, #VE cannot be used
    because the IDT needed for handling the exception is not set-up, and
    some other infrastructure needed by the handler is missing. So to
    support port IO in decompressor code, directly replace in/out
    instructions with TDX IO hypercalls. This can beeasily achieved by
    modifying __in/__out macros.
    
    Also, since TDX IO hypercall requires an IO size parameter, modify
    __in/__out macros to accept size as input parameter.
    
    Reviewed-by: Andi Kleen <ak@linux.intel.com>
    Reviewed-by: Dan Williams <dan.j.williams@intel.com>
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

--- arch/x86/include/asm/tdx.h
+++ arch/x86/include/asm/tdx.h
@@ -5,6 +5,8 @@
 
 #include <linux/cpufeature.h>
 #include <linux/types.h>
+#include <vdso/limits.h>
+#include <asm/vmx.h>
 
 #define TDX_CPUID_LEAF_ID	0x21
 
@@ -69,6 +71,54 @@ unsigned long tdg_get_ve_info(struct ve_info *ve);
 
 int tdg_handle_virtualization_exception(struct pt_regs *regs,
 		struct ve_info *ve);
+
+/*
+ * To support I/O port access in decompressor or early kernel init
+ * code, since #VE exception handler cannot be used, use paravirt
+ * model to implement __in/__out macros which will in turn be used
+ * by in{b,w,l}()/out{b,w,l} I/O helper macros used in kernel. You
+ * can find the __in/__out macro usage in arch/x86/include/asm/io.h
+ */
+#ifdef BOOT_COMPRESSED_MISC_H
+
+bool early_is_tdx_guest(void);
+
+/*
+ * Helper function used for making hypercall for "in"
+ * instruction. It will be called from __in IO macro
+ * If IO is failed, it will return all 1s.
+ */
+static inline unsigned int tdg_in(int size, int port)
+{
+	struct tdx_hypercall_output out = {0};
+	int err;
+
+	err = __tdx_hypercall(EXIT_REASON_IO_INSTRUCTION, size, 0,
+			      port, 0, &out);
+
+	return err ? UINT_MAX : out.r11;
+}
+
+#define __out(bwl, bw, sz)						\
+do {									\
+	if (early_is_tdx_guest()) {					\
+		__tdx_hypercall(EXIT_REASON_IO_INSTRUCTION, sz, 1,	\
+				port, value, NULL);			\
+	} else {							\
+		asm volatile("out" #bwl " %" #bw "0, %w1" : :		\
+				"a"(value), "Nd"(port));		\
+	}								\
+} while (0)
+#define __in(bwl, bw, sz)						\
+do {									\
+	if (early_is_tdx_guest()) {					\
+		value = tdg_in(sz, port);				\
+	} else {							\
+		asm volatile("in" #bwl " %w1, %" #bw "0" :		\
+				"=a"(value) : "Nd"(port));		\
+	}								\
+} while (0)
+#endif
 
 #else
 
