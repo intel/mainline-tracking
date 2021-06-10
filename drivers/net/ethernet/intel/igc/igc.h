749075ce532e1614f00d854b201351da271b38c8
Muhammad Husaini Zulkifli <muhammad.husaini.zulkifli@intel.com>
2021-06-10 12:44:46 +0800

    igc: Enable HW TX Timestamp for AF_XDP ZC
    Enable HW TX Timestamp per-packet using dma write back descriptor.
    
    Signed-off-by: Muhammad Husaini Zulkifli <muhammad.husaini.zulkifli@intel.com>
    Signed-off-by: Aravindhan Gunasekaran <aravindhan.gunasekaran@intel.com>

--- drivers/net/ethernet/intel/igc/igc.h
+++ drivers/net/ethernet/intel/igc/igc.h
@@ -790,6 +790,7 @@ void igc_ptp_clear_xsk_tx_tstamp_queue(struct igc_adapter *adapter,
 				       u16 queue_id);
 void igc_ptp_read(struct igc_adapter *adapter, struct timespec64 *ts);
 void igc_ptp_tx_tstamp_event(struct igc_adapter *adapter);
+ktime_t igc_tx_dma_hw_tstamp(struct igc_adapter *adapter, u64 tstamp);
 
 int igc_led_setup(struct igc_adapter *adapter);
 void igc_led_free(struct igc_adapter *adapter);
