intel/tdx

x86/tdx: Implement port I/O filtering
Add allowlist of ports that can be used for communication with the host.
Only RTC and PCI ranges are essential. For debug mode we also allow COM1
to be used for console as virtio console get initialized late.

The filtering is disabled for decompression code. It doesn't have
significant attack surface to care about.

Signed-off-by: Kirill A. Shutemov <kirill.shutemov@linux.intel.com>
Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

arch/x86/include/asm/tdx.h
@@ -32,6 +32,8 @@ static inline bool is_tdx_guest(void)
 bool tdx_debug_enabled(void);
 void __init tdx_early_init(void);
 
+bool tdx_allowed_port(short int port);
+
 /* Decompression code doesn't know how to handle alternatives */
 #ifdef BOOT_COMPRESSED_MISC_H
 #define __out(bwl, bw)							\

intel/mainline-tracking

Reviewed-by: Andi Kleen <ak@linux.intel.com>

arch/x86/include/asm/tdx.h
@@ -84,6 +84,7 @@ int tdg_handle_virtualization_exception(struct pt_regs *regs,
 		struct ve_info *ve);
 
 bool tdg_early_handle_ve(struct pt_regs *regs);
+bool tdg_allowed_port(short int port);
 
 extern phys_addr_t tdg_shared_mask(void);
 