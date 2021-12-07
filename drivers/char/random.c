ba079c987910cd21c3b6a38cf8d3ae42e986d689
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2021-12-07 13:17:37 +0100

    random: Defer processing of randomness on PREEMPT_RT.
    On interrupt invocation, add_interrupt_randomness() adds entropy to its
    per-CPU state and if it gathered enough of it then it will mix it into a
    entropy_store. In order to do so, it needs to lock the pool by acquiring
    entropy_store::lock which is a spinlock_t. This lock can not be acquired
    on PREEMPT_RT with disabled interrupts because it is a sleeping lock.
    
    This lock could be made a raw_spinlock_t which will then allow to
    acquire it with disabled interrupts on PREEMPT_RT. The lock is usually
    hold for short amount of cycles while entropy is added to the pool and
    the invocation from the IRQ handler has a try-lock which avoids spinning
    on the lock if contended. The extraction of entropy (extract_buf())
    needs a few cycles more because it performs additionally few
    SHA1 transformations. This takes around 5-10us on a testing box (E5-2650
    32 Cores, 2way NUMA) and is negligible.
    
    The frequent invocation of the IOCTLs RNDADDTOENTCNT and RNDRESEEDCRNG
    on multiple CPUs in parallel leads to filling and depletion of the pool
    which in turn results in heavy contention on the lock. The spinning with
    disabled interrupts on multiple CPUs leads to latencies of at least
    100us on the same machine which is no longer acceptable.
    
    Collect only the IRQ randomness in IRQ-context on PREEMPT_RT.
    In threaded-IRQ context, make a copy of the per-CPU state with disabled
    interrupts to ensure that it is not modified while duplicated. Pass the
    copy to process_interrupt_randomness_pool() and reset the per-CPU
    afterwards if needed.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Link: https://lore.kernel.org/r/20211207121737.2347312-6-bigeasy@linutronix.de

--- drivers/char/random.c
+++ drivers/char/random.c
@@ -1245,6 +1245,32 @@ static bool process_interrupt_randomness_pool(struct fast_pool *fast_pool)
 	return true;
 }
 
+#ifdef CONFIG_PREEMPT_RT
+void process_interrupt_randomness(void)
+{
+	struct fast_pool *cpu_pool;
+	struct fast_pool fast_pool;
+
+	lockdep_assert_irqs_enabled();
+
+	migrate_disable();
+	cpu_pool = this_cpu_ptr(&irq_randomness);
+
+	local_irq_disable();
+	memcpy(&fast_pool, cpu_pool, sizeof(fast_pool));
+	local_irq_enable();
+
+	if (process_interrupt_randomness_pool(&fast_pool)) {
+		local_irq_disable();
+		cpu_pool->last = jiffies;
+		cpu_pool->count = 0;
+		local_irq_enable();
+	}
+	memzero_explicit(&fast_pool, sizeof(fast_pool));
+	migrate_enable();
+}
+#endif
+
 void add_interrupt_randomness(int irq)
 {
 	struct fast_pool *fast_pool = this_cpu_ptr(&irq_randomness);
@@ -1268,7 +1294,13 @@ void add_interrupt_randomness(int irq)
 	fast_mix(fast_pool);
 	add_interrupt_bench(cycles);
 
-	if (process_interrupt_randomness_pool(fast_pool)) {
+	/*
+	 * On PREEMPT_RT the entropy can not be fed into the input_pool because
+	 * it needs to acquire sleeping locks with disabled interrupts.
+	 * This is deferred to the threaded handler.
+	 */
+	if (!IS_ENABLED(CONFIG_PREEMPT_RT) &&
+	    process_interrupt_randomness_pool(fast_pool)) {
 		fast_pool->last = now;
 		fast_pool->count = 0;
 	}
