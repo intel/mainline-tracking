87ae788a3e9b02b321acc71faa12d9f98da3d1fb
Mazlan, Hazwan Arif <hazwan.arif.mazlan@intel.com>
2022-03-15 09:10:53 +0800

    drivers/gpu/drm/i915/rpl-p: RPL-P Enablement
    Bspec: 55376
    RPL-P behaves identically to ADL-P from i915's point of view.
    
    Signed-off-by: Mazlan, Hazwan Arif <hazwan.arif.mazlan@intel.com>

--- drivers/gpu/drm/i915/intel_device_info.h
+++ drivers/gpu/drm/i915/intel_device_info.h
@@ -111,7 +111,7 @@ enum intel_platform {
 #define INTEL_SUBPLATFORM_G11	1
 
 /* ADL-S */
-#define INTEL_SUBPLATFORM_RPL_S	0
+#define INTEL_SUBPLATFORM_RPL	0
 
 enum intel_ppgtt_type {
 	INTEL_PPGTT_NONE = I915_GEM_PPGTT_NONE,
