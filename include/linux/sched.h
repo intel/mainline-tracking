069a15d0518ae96d45c947567d976f450bc24071
Thomas Gleixner <tglx@linutronix.de>
2022-04-13 15:31:02 +0200

    sched: Fix missing prototype warnings
    A W=1 build emits more than a dozen missing prototype warnings related to
    scheduler and scheduler specific includes.
    
    Reported-by: kernel test robot <lkp@intel.com>
    Signed-off-by: Thomas Gleixner <tglx@linutronix.de>
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>
    Link: https://lore.kernel.org/r/20220413133024.249118058@linutronix.de

--- include/linux/sched.h
+++ include/linux/sched.h
@@ -2364,4 +2364,6 @@ static inline void sched_core_free(struct task_struct *tsk) { }
 static inline void sched_core_fork(struct task_struct *p) { }
 #endif
 
+extern void sched_set_stop_task(int cpu, struct task_struct *stop);
+
 #endif
