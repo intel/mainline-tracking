23b3ed23808124f57d990d6e4d64810e5fd925e8
Gayatri Kammela <gayatri.kammela@linux.intel.com>
2022-08-16 13:27:25 -0700

    platform/x86: intel/pmc: Add Raptor Lake Desktop support to Intel PMC core
    Add RPL Desktop support to Intel's PMC core driver.
    
    Signed-off-by: Gayatri Kammela <gayatri.kammela@linux.intel.com>

--- drivers/platform/x86/intel/pmc/core.c
+++ drivers/platform/x86/intel/pmc/core.c
@@ -1943,6 +1943,7 @@ static const struct x86_cpu_id intel_pmc_core_ids[] = {
 	X86_MATCH_INTEL_FAM6_MODEL(METEORLAKE,		&mtl_reg_map),
 	X86_MATCH_INTEL_FAM6_MODEL(METEORLAKE_L,	&mtl_reg_map),
 	X86_MATCH_INTEL_FAM6_MODEL(RAPTORLAKE,		&adl_reg_map),
+	X86_MATCH_INTEL_FAM6_MODEL(RAPTORLAKE_S,	&adl_reg_map),
 	{}
 };
 
