intel/mainline-tracking

commit f24eb93489220bf70d05b72a5e02be766aeced16

net: phy: marvell10g: Add WAKE_PHY support to WOL event
Add Wake-on-PHY feature support by enabling the Link Status Changed
interrupt.

Signed-off-by: Song Yoong Siang <yoong.siang.song@intel.com>

drivers/net/phy/marvell10g.c
@@ -88,6 +88,11 @@ enum {
 	MV_PCS_CSSR1_SPD2_2500	= 0x0004,
 	MV_PCS_CSSR1_SPD2_10000	= 0x0000,
 
+	/* Copper Specific Interrupt registers */
+	MV_PCS_INTR_ENABLE	= 0x8010,
+	MV_PCS_INTR_ENABLE_LSC	= BIT(10),
+	MV_PCS_INTR_STS		= 0x8011,
+
 	/* Temperature read register (88E2110 only) */
 	MV_PCS_TEMP		= 0x8042,
 
@@ -1149,7 +1154,7 @@ static void mv3110_get_wol(struct phy_device *phydev,
 {
 	int ret;
 
-	wol->supported = WAKE_MAGIC;
+	wol->supported = WAKE_MAGIC | WAKE_PHY;
 	wol->wolopts = 0;
 
 	ret = phy_read_mmd(phydev, MDIO_MMD_VEND2, MV_V2_WOL_CTRL);
@@ -1158,6 +1163,13 @@ static void mv3110_get_wol(struct phy_device *phydev,
 
 	if (ret & MV_V2_WOL_CTRL_MAGIC_PKT_EN)
 		wol->wolopts |= WAKE_MAGIC;
+
+	ret = phy_read_mmd(phydev, MDIO_MMD_PCS, MV_PCS_INTR_ENABLE);
+	if (ret < 0)
+		return;
+
+	if (ret & MV_PCS_INTR_ENABLE_LSC)
+		wol->wolopts |= WAKE_PHY;
 }
 
 static int mv3110_set_wol(struct phy_device *phydev,
@@ -1208,6 +1220,25 @@ static int mv3110_set_wol(struct phy_device *phydev,
 				     MV_V2_WOL_CTRL,
 				     MV_V2_WOL_CTRL_MAGIC_PKT_EN,
 				     MV_V2_WOL_CTRL_CLEAR_STS);
+		if (ret < 0)
+			return ret;
+	}
+
+	if (wol->wolopts & WAKE_PHY) {
+		/* Enable the link status changed interrupt */
+		ret = phy_set_bits_mmd(phydev, MDIO_MMD_PCS,
+				       MV_PCS_INTR_ENABLE,
+				       MV_PCS_INTR_ENABLE_LSC);
+		if (ret < 0)
+			return ret;
+
+		/* Clear the interrupt status register */
+		ret = phy_read_mmd(phydev, MDIO_MMD_PCS, MV_PCS_INTR_STS);
+	} else {
+		/* Disable the link status changed interrupt */
+		ret = phy_clear_bits_mmd(phydev, MDIO_MMD_PCS,
+					 MV_PCS_INTR_ENABLE,
+					 MV_PCS_INTR_ENABLE_LSC);
 		if (ret < 0)
 			return ret;
 	}

commit 4e428d25616e683e3cc066cc132cc15d9735dd6b

net: phy: marvell10g: Add check after reading phy interrupt status
Add return check after reading phy interrupt status to exit properly
when exception occurs.

Fixes: f24eb9348922 ("net: phy: marvell10g: Add WAKE_PHY support to WOL event")
Signed-off-by: Michael Sit Wei Hong <michael.wei.hong.sit@intel.com>

drivers/net/phy/marvell10g.c
@@ -1234,6 +1234,8 @@ static int mv3110_set_wol(struct phy_device *phydev,
 
 		/* Clear the interrupt status register */
 		ret = phy_read_mmd(phydev, MDIO_MMD_PCS, MV_PCS_INTR_STS);
+		if (ret < 0)
+			return ret;
 	} else {
 		/* Disable the link status changed interrupt */
 		ret = phy_clear_bits_mmd(phydev, MDIO_MMD_PCS,