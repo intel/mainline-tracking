8d027387a24d9d3beb899c1513da29324b09884a
James Xiong <james.xiong@intel.com>
2022-11-16 15:29:41 -0800

    drm/i915: use the original Wa_14010685332 for PCH_ADP
    Bspec: 33450 lists 2 sequences for Wa_14010685332.
    The original sequence was implemented for PCH_ICP+
    commit b896898c7369 ("drm/i915: Tweaked Wa_14010685332 for PCHs
                          used on gen11 platforms")
    commit b8441b288d60 ("drm/i915: Tweaked Wa_14010685332 for all PCHs")
    removed the original and applied the tweaked one to all PCHs(CNP+),
    and caused regressions on ADP.
    
    Fixes: b8441b288d60 ("drm/i915: Tweaked Wa_14010685332 for all PCHs")
    Signed-off-by: James Xiong <james.xiong@intel.com>

--- drivers/gpu/drm/i915/display/intel_display_power.c
+++ drivers/gpu/drm/i915/display/intel_display_power.c
@@ -2254,9 +2254,14 @@ void intel_display_power_suspend_late(struct intel_display *display, bool s2idle
 		hsw_enable_pc8(display);
 	}
 
-	/* Tweaked Wa_14010685332:cnp,icp,jsp,mcc,tgp,adp */
-	if (INTEL_PCH_TYPE(i915) >= PCH_CNP && INTEL_PCH_TYPE(i915) < PCH_DG1)
+	/* Tweaked Wa_14010685332:cnp,icp,jsp,mcc,tgp */
+	if (INTEL_PCH_TYPE(i915) >= PCH_CNP && INTEL_PCH_TYPE(i915) < PCH_DG1) {
 		intel_de_rmw(i915, SOUTH_CHICKEN1, SBCLK_RUN_REFCLK_DIS, SBCLK_RUN_REFCLK_DIS);
+
+		/* Original Wa_14010685332: adp */
+		if (INTEL_PCH_TYPE(i915) == PCH_ADP)
+			intel_de_rmw(i915, SOUTH_CHICKEN1, SBCLK_RUN_REFCLK_DIS, 0);
+	}
 }
 
 void intel_display_power_resume_early(struct intel_display *display)
@@ -2274,8 +2279,9 @@ void intel_display_power_resume_early(struct intel_display *display)
 		hsw_disable_pc8(display);
 	}
 
-	/* Tweaked Wa_14010685332:cnp,icp,jsp,mcc,tgp,adp */
-	if (INTEL_PCH_TYPE(i915) >= PCH_CNP && INTEL_PCH_TYPE(i915) < PCH_DG1)
+	/* Tweaked Wa_14010685332:cnp,icp,jsp,mcc,tgp */
+	if ((INTEL_PCH_TYPE(i915) >= PCH_CNP && INTEL_PCH_TYPE(i915) < PCH_DG1) &&
+	    INTEL_PCH_TYPE(i915) != PCH_ADP)
 		intel_de_rmw(i915, SOUTH_CHICKEN1, SBCLK_RUN_REFCLK_DIS, 0);
 
 	intel_power_domains_resume(display);
