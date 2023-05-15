374ddb3c28f87a579117fbe64504aa00b8393235
Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
2023-05-15 14:20:21 +0300

    ASoC: SOF: Intel: hda-dai-ops: Split the get_hext_stream() op for IPC4
    Introduce a separate op implementation for get_hext_stream() for IPC4.
    This op will also be used to set the skip_during_fe_trigger flag for the
    BE DAI pipeline. With this change, we can remove the flag setting in
    sof_ipc4_dai_config() which will further simplify support for
    DMIC/SSP/Soundwire in the LunarLake platform.
    
    Signed-off-by: Ranjani Sridharan <ranjani.sridharan@linux.intel.com>
    Reviewed-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Reviewed-by: Bard Liao <yung-chuan.liao@linux.intel.com>
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@linux.intel.com>
    Link: https://lore.kernel.org/r/20230515112022.30297-2-peter.ujfalusi@linux.intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>

--- sound/soc/sof/ipc4-topology.c
+++ sound/soc/sof/ipc4-topology.c
@@ -2503,7 +2503,6 @@ static int sof_ipc4_dai_config(struct snd_sof_dev *sdev, struct snd_sof_widget *
 	case SOF_DAI_INTEL_HDA:
 		gtw_attr = ipc4_copier->gtw_attr;
 		gtw_attr->lp_buffer_alloc = pipeline->lp_mode;
-		pipeline->skip_during_fe_trigger = true;
 		fallthrough;
 	case SOF_DAI_INTEL_ALH:
 		/*
