6330fd67426899fee44f33fe7c515411e168963a
Xin Li <xin3.li@intel.com>
2023-08-31 00:56:30 -0700

    KVM: VMX: Set FRED MSR interception
    Add FRED MSRs to the VMX passthrough MSR list and set FRED MSRs
    interception.
    
    8 FRED MSRs, i.e., MSR_IA32_FRED_RSP[123], MSR_IA32_FRED_STKLVLS,
    MSR_IA32_FRED_SSP[123] and MSR_IA32_FRED_CONFIG, are all safe to
    be passthrough, because they all have a pair of corresponding host
    and guest VMCS fields.
    
    Both MSR_IA32_FRED_RSP0 and MSR_IA32_FRED_SSP0 are dedicated for
    userspace event delivery only, IOW they are NOT used in any kernel
    event delivery and the execution of ERETS.  Thus KVM can run safely
    with guest values in the 2 MSRs.  As a result, save and restore of
    their guest values are deferred until vCPU context switch and their
    host values are restored upon host returning to userspace.
    
    Signed-off-by: Xin Li <xin3.li@intel.com>
    Signed-off-by: Xin Li (Intel) <xin@zytor.com>
    Tested-by: Shan Kang <shan.kang@intel.com>
    Tested-by: Xuelian Guo <xuelian.guo@intel.com>
    ---
    
    Changes in v5:
    * Skip execution of vmx_set_intercept_for_fred_msr() if FRED is
      not available or enabled (Sean).
    * Use 'set' instead of 'flag' as the variable name to indicate
      whether MSR interception should be enabled (Sean).
    * Add TB from Xuelian Guo.

--- arch/x86/kvm/vmx/vmx.h
+++ arch/x86/kvm/vmx/vmx.h
@@ -297,7 +297,7 @@ struct vcpu_vmx {
 	struct lbr_desc lbr_desc;
 
 	/* Save desired MSR intercept (read: pass-through) state */
-#define MAX_POSSIBLE_PASSTHROUGH_MSRS	16
+#define MAX_POSSIBLE_PASSTHROUGH_MSRS	26
 	struct {
 		DECLARE_BITMAP(read, MAX_POSSIBLE_PASSTHROUGH_MSRS);
 		DECLARE_BITMAP(write, MAX_POSSIBLE_PASSTHROUGH_MSRS);
