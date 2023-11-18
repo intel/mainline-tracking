c26dd7e5703d67fdc3b9c87a1a100f3ae0830dea
Xin Li <xin3.li@intel.com>
2023-11-17 21:34:27 -0800

    KVM: VMX: Disable FRED if FRED consistency checks fail
    Do not virtualize FRED if FRED consistency checks fail.
    
    Either on broken hardware, or when run KVM on top of another hypervisor
    before the underlying hypervisor implements nested FRED correctly.
    
    Suggested-by: Chao Gao <chao.gao@intel.com>
    Signed-off-by: Xin Li <xin3.li@intel.com>
    Signed-off-by: Xin Li (Intel) <xin@zytor.com>
    Reviewed-by: Chao Gao <chao.gao@intel.com>
    Tested-by: Shan Kang <shan.kang@intel.com>
    Tested-by: Xuelian Guo <xuelian.guo@intel.com>
    ---
    
    Changes in v5:
    * Drop the cpu_feature_enabled() in cpu_has_vmx_fred() (Sean).
    * Add TB from Xuelian Guo.
    
    Change in v4:
    * Call out the reason why not check FRED VM-exit controls in
      cpu_has_vmx_fred() (Chao Gao).

--- arch/x86/kvm/vmx/capabilities.h
+++ arch/x86/kvm/vmx/capabilities.h
@@ -400,6 +400,16 @@ static inline bool vmx_pebs_supported(void)
 	return boot_cpu_has(X86_FEATURE_PEBS) && kvm_pmu_cap.pebs_ept;
 }
 
+static inline bool cpu_has_vmx_fred(void)
+{
+	/*
+	 * setup_vmcs_config() guarantees FRED VM-entry/exit controls
+	 * are either all set or none.  So, no need to check FRED VM-exit
+	 * controls.
+	 */
+	return (vmcs_config.vmentry_ctrl & VM_ENTRY_LOAD_IA32_FRED);
+}
+
 static inline bool cpu_has_notify_vmexit(void)
 {
 	return vmcs_config.cpu_based_2nd_exec_ctrl &
