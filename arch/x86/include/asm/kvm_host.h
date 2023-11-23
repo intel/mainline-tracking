64e7e822c4c3535413b83c11732b59a0b6a2fff9
Zeng Guang <guang.zeng@intel.com>
2023-11-23 23:53:55 +0800

    KVM: VMX: Implement NMI source injection
    With NMI source the vector field along with NMI encoding to trigger NMI through
    APIC ICR and LVT register can be repurposed to identify the originator (source)
    of the NMI. NMI source vector is delivered when FRED is enabled and reported as
    an accumulated 16-bit bitmask in the exception event data field pushed on the
    stack for a FRED exception.
    
    Introduce two new elements in struct kvm_vcpu_arch used for NMI source injection.
    "nmi_source_pending" collects multiple NMIs from different sources arriving at
    local APIC simultanously and coalesces into a 16-bit bitmask. "nmi_source_inject"
    indicates the NMI sources that will be delivered in current NMI injection.
    
    KVM injects NMI source into the guest by specifying the NMI source in the injected
    event data field of the VMCS.
    
    Signed-off-by: Zeng Guang <guang.zeng@intel.com>

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -969,6 +969,8 @@ struct kvm_vcpu_arch {
 	u64 tsc_scaling_ratio; /* current scaling ratio */
 
 	atomic_t nmi_queued;  /* unprocessed asynchronous NMIs */
+	atomic_t nmi_source_pending;  /* unprocessed NMI Source */
+	unsigned int nmi_source_inject;  /* NMI Source to inject */
 	/* Number of NMIs pending injection, not including hardware vNMIs. */
 	unsigned int nmi_pending;
 	bool nmi_injected;    /* Trying to inject an NMI this entry */
