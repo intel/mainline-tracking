0605debd1757c1843ea3b778ddb64a1694439745
Rodrigo Vivi <rodrigo.vivi@intel.com>
2023-12-08 12:11:06 -0500

    drm/xe: Add PVC's PCI device IDs
    This patch introduces Ponte Vecchio support in Xe driver.
    
    Please note that besides this patch, likely the force_probe
    still needs to be used in order to actually enable the
    support for PVC.
    
    This patch was separated from the rest so we can ensure
    compliance with DRM uAPI rules on compute platforms.
    
    Cc: Lucas De Marchi <lucas.demarchi@intel.com>
    Cc: Thomas Hellström <thomas.hellstrom@linux.intel.com>
    Cc: Oded Gabbay <ogabbay@kernel.org>
    Link: https://gitlab.freedesktop.org/drm/xe/kernel/-/issues/1154
    Signed-off-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Reviewed-by: Lucas De Marchi <lucas.demarchi@intel.com>
    Signed-off-by: Thomas Hellström <thomas.hellstrom@linux.intel.com>
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>
    Signed-off-by: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>

--- drivers/gpu/drm/xe/xe_pci.c
+++ drivers/gpu/drm/xe/xe_pci.c
@@ -328,7 +328,7 @@ static const struct xe_device_desc dg2_desc = {
 	.has_display = true,
 };
 
-static const __maybe_unused struct xe_device_desc pvc_desc = {
+static const struct xe_device_desc pvc_desc = {
 	.graphics = &graphics_xehpc,
 	DGFX_FEATURES,
 	PLATFORM(PVC),
@@ -414,6 +414,7 @@ static const struct pci_device_id pciidlist[] = {
 	INTEL_ATS_M_IDS(INTEL_VGA_DEVICE, &ats_m_desc),
 	INTEL_ARL_IDS(INTEL_VGA_DEVICE, &mtl_desc),
 	INTEL_DG2_IDS(INTEL_VGA_DEVICE, &dg2_desc),
+	INTEL_PVC_IDS(INTEL_VGA_DEVICE, &pvc_desc),
 	INTEL_MTL_IDS(INTEL_VGA_DEVICE, &mtl_desc),
 	INTEL_LNL_IDS(INTEL_VGA_DEVICE, &lnl_desc),
 	INTEL_BMG_IDS(INTEL_VGA_DEVICE, &bmg_desc),
