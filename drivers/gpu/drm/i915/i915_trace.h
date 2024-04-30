64966e27a29db4cc736124c9aa81bb4722a73ba7
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2024-04-30 12:53:28 +0200

    i915: Disable tracepoints (again).
    The tracepoints in i915 were enabled after some of them were moved
    into a different header file. This has been noticed while posting
    the series for review and now the tracepoints are disabled again.
    The problem is that some of them acquire sleeping locks within the
    tracepoint which is invoked with disabled preemption.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- drivers/gpu/drm/i915/i915_trace.h
+++ drivers/gpu/drm/i915/i915_trace.h
@@ -6,7 +6,7 @@
 #if !defined(_I915_TRACE_H_) || defined(TRACE_HEADER_MULTI_READ)
 #define _I915_TRACE_H_
 
-#ifdef CONFIG_PREEMPT_RT
+#if defined(CONFIG_PREEMPT_RT) && !defined(NOTRACE)
 #define NOTRACE
 #endif
 
@@ -326,7 +326,7 @@ DEFINE_EVENT(i915_request, i915_request_add,
 	     TP_ARGS(rq)
 );
 
-#if defined(CONFIG_DRM_I915_LOW_LEVEL_TRACEPOINTS) && !defined(NOTRACE)
+#if defined(CONFIG_DRM_I915_LOW_LEVEL_TRACEPOINTS)
 DEFINE_EVENT(i915_request, i915_request_guc_submit,
 	     TP_PROTO(struct i915_request *rq),
 	     TP_ARGS(rq)
