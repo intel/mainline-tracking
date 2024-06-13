d4da66be58257c5d8c78c6ee74ff28e5111317de
Xin Li (Intel) <xin@zytor.com>
2024-06-13 09:48:43 -0700

    KVM: x86: Save/restore the nested flag of an exception
    Save/restore the nested flag of an exception during VM save/restore
    and live migration to ensure a correct event stack level is chosen
    when a nested exception is injected through FRED event delivery.
    
    Signed-off-by: Xin Li (Intel) <xin@zytor.com>
    Tested-by: Xuelian Guo <xuelian.guo@intel.com>
    ---
    
    Change in v8:
    * Update KVM_CAP_EXCEPTION_NESTED_FLAG, as the number in v7 is used
      by another new cap.
    
    Change in v5:
    * Add TB from Xuelian Guo.
    
    Change in v4:
    * Add live migration support for exception nested flag (Chao Gao).

--- include/uapi/linux/kvm.h
+++ include/uapi/linux/kvm.h
@@ -974,6 +974,7 @@ struct kvm_enable_cap {
 #define KVM_CAP_GUEST_MEMFD_FLAGS 244
 #define KVM_CAP_ARM_SEA_TO_USER 245
 #define KVM_CAP_S390_USER_OPEREXEC 246
+#define KVM_CAP_EXCEPTION_NESTED_FLAG 247
 
 struct kvm_irq_routing_irqchip {
 	__u32 irqchip;
