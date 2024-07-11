b80aee82e05a067ba83980fc3b01a446ca29a7c1
Michal Wajdeczko <michal.wajdeczko@intel.com>
2024-07-11 21:23:20 +0200

    drm/xe: Enable SR-IOV for ADL/ATSM
    We should now have sufficient changes in the Xe driver changes to
    run it on ADL and ATSM platforms in the PF mode, to configure VFs
    and successfully probe driver on the enabled VF devices.
    
    While some more changes are likely still needed to fix all corner
    cases, we will not find them without running any tests. To start
    testing this feature by the CI, we need to mark which platforms
    have basic SR-IOV support and let the driver run in the PF mode.
    
    Since this feature support is still in the early testing stage,
    make all enabling available only for CONFIG_DRM_XE_DEBUG=y and
    keep it on CI topic branch.
    
    Note that from this point, on selected platforms, the Xe driver
    will be acting as a PF driver, will some SR-IOV specific changes
    compared to running in the non-virtualized (native) mode.
    
    However, those specific changes are visible mostly on the debugfs,
    and should not impact normal driver execution, unless VFs will be
    manually provisioned or explicitly enabled.
    
    Once we finish adding the remaining SR-IOV tests to the CI and fix
    any issues that we find in the meantime, we will replace this patch
    with proper series outside the topic branch.
    
    Suggested-by: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Signed-off-by: Michal Wajdeczko <michal.wajdeczko@intel.com>
    Acked-by: Lucas De Marchi <lucas.demarchi@intel.com>
    Acked-by: Thomas Hellstrom <thomas.hellstrom@linux.intel.com>
    Reviewed-by: Jonathan Cavitt <jonathan.cavitt@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20240711192320.1198-3-michal.wajdeczko@intel.com
    Signed-off-by: Lucas De Marchi <lucas.demarchi@intel.com>
    Signed-off-by: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>

--- drivers/gpu/drm/xe/xe_pci.c
+++ drivers/gpu/drm/xe/xe_pci.c
@@ -242,6 +242,7 @@ static const struct xe_device_desc adl_s_desc = {
 	.dma_mask_size = 39,
 	.has_display = true,
 	.has_llc = true,
+	.has_sriov = IS_ENABLED(CONFIG_DRM_XE_DEBUG),
 	.require_force_probe = true,
 	.subplatforms = (const struct xe_subplatform_desc[]) {
 		{ XE_SUBPLATFORM_ALDERLAKE_S_RPLS, "RPLS", adls_rpls_ids },
@@ -258,6 +259,7 @@ static const struct xe_device_desc adl_p_desc = {
 	.dma_mask_size = 39,
 	.has_display = true,
 	.has_llc = true,
+	.has_sriov = IS_ENABLED(CONFIG_DRM_XE_DEBUG),
 	.require_force_probe = true,
 	.subplatforms = (const struct xe_subplatform_desc[]) {
 		{ XE_SUBPLATFORM_ALDERLAKE_P_RPLU, "RPLU", adlp_rplu_ids },
@@ -272,6 +274,7 @@ static const struct xe_device_desc adl_n_desc = {
 	.dma_mask_size = 39,
 	.has_display = true,
 	.has_llc = true,
+	.has_sriov = IS_ENABLED(CONFIG_DRM_XE_DEBUG),
 	.require_force_probe = true,
 };
 
@@ -312,6 +315,7 @@ static const struct xe_device_desc ats_m_desc = {
 
 	DG2_FEATURES,
 	.has_display = false,
+	.has_sriov = IS_ENABLED(CONFIG_DRM_XE_DEBUG),
 };
 
 static const struct xe_device_desc dg2_desc = {
