torvalds/linux

commit 61572d5f8f91d8603d8db028e1ec9e18fc0ca245

igc: Simplify TSN flags handling
Separates the procedure done during reset from applying a
configuration, knowing when the code is executing allow us to
separate the better what changes the hardware state from what
changes only the driver state.

Introduces a flag for bookkeeping the driver state of TSN
features. When Qav and frame-preemption is also implemented
this flag makes it easier to keep track on whether a TSN feature
driver state is enabled or not though controller state changes,
say, during a reset.

Signed-off-by: Vinicius Costa Gomes <vinicius.gomes@intel.com>
Signed-off-by: Aravindhan Gunasekaran <aravindhan.gunasekaran@intel.com>
Signed-off-by: Mallikarjuna Chilakala <mallikarjuna.chilakala@intel.com>
Tested-by: Dvora Fuxbrumer <dvorax.fuxbrumer@linux.intel.com>
Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>

drivers/net/ethernet/intel/igc/igc_main.c
@@ -120,7 +120,7 @@ void igc_reset(struct igc_adapter *adapter)
 	igc_ptp_reset(adapter);
 
 	/* Re-enable TSN offloading, where applicable. */
-	igc_tsn_offload_apply(adapter);
+	igc_tsn_reset(adapter);
 
 	igc_get_phy_info(hw);
 }

drivers/net/ethernet/intel/igc/igc_tsn.c
@@ -18,19 +18,27 @@ static bool is_any_launchtime(struct igc_adapter *adapter)
 	return false;
 }
 
+static unsigned int igc_tsn_new_flags(struct igc_adapter *adapter)
+{
+	unsigned int new_flags = adapter->flags & ~IGC_FLAG_TSN_ANY_ENABLED;
+
+	if (adapter->base_time)
+		new_flags |= IGC_FLAG_TSN_QBV_ENABLED;
+
+	if (is_any_launchtime(adapter))
+		new_flags |= IGC_FLAG_TSN_QBV_ENABLED;
+
+	return new_flags;
+}
+
 /* Returns the TSN specific registers to their default values after
- * TSN offloading is disabled.
+ * the adapter is reset.
  */
 static int igc_tsn_disable_offload(struct igc_adapter *adapter)
 {
 	struct igc_hw *hw = &adapter->hw;
 	u32 tqavctrl;
 	int i;
-
-	if (!(adapter->flags & IGC_FLAG_TSN_QBV_ENABLED))
-		return 0;
-
-	adapter->cycle_time = 0;
 
 	wr32(IGC_TXPBS, I225_TXPBSIZE_DEFAULT);
 	wr32(IGC_DTXMXPKTSZ, IGC_DTXMXPKTSZ_DEFAULT);
@@ -61,9 +69,6 @@ static int igc_tsn_enable_offload(struct igc_adapter *adapter)
 	u32 sec, nsec, cycle;
 	ktime_t base_time, systim;
 	int i;
-
-	if (adapter->flags & IGC_FLAG_TSN_QBV_ENABLED)
-		return 0;
 
 	cycle = adapter->cycle_time;
 	base_time = adapter->base_time;
@@ -119,33 +124,41 @@ static int igc_tsn_enable_offload(struct igc_adapter *adapter)
 	wr32(IGC_BASET_H, baset_h);
 	wr32(IGC_BASET_L, baset_l);
 
-	adapter->flags |= IGC_FLAG_TSN_QBV_ENABLED;
-
 	return 0;
+}
+
+int igc_tsn_reset(struct igc_adapter *adapter)
+{
+	unsigned int new_flags;
+	int err = 0;
+
+	new_flags = igc_tsn_new_flags(adapter);
+
+	if (!(new_flags & IGC_FLAG_TSN_ANY_ENABLED))
+		return igc_tsn_disable_offload(adapter);
+
+	err = igc_tsn_enable_offload(adapter);
+	if (err < 0)
+		return err;
+
+	adapter->flags = new_flags;
+
+	return err;
 }
 
 int igc_tsn_offload_apply(struct igc_adapter *adapter)
 {
-	bool is_any_enabled = adapter->base_time || is_any_launchtime(adapter);
+	int err;
 
-	if (!(adapter->flags & IGC_FLAG_TSN_QBV_ENABLED) && !is_any_enabled)
-		return 0;
-
-	if (!is_any_enabled) {
-		int err = igc_tsn_disable_offload(adapter);
-
-		if (err < 0)
-			return err;
-
-		/* The BASET registers aren't cleared when writing
-		 * into them, force a reset if the interface is
-		 * running.
-		 */
-		if (netif_running(adapter->netdev))
-			schedule_work(&adapter->reset_task);
-
+	if (netif_running(adapter->netdev)) {
+		schedule_work(&adapter->reset_task);
 		return 0;
 	}
 
-	return igc_tsn_enable_offload(adapter);
+	err = igc_tsn_enable_offload(adapter);
+	if (err < 0)
+		return err;
+
+	adapter->flags = igc_tsn_new_flags(adapter);
+	return 0;
 }

drivers/net/ethernet/intel/igc/igc_tsn.h
@@ -5,5 +5,6 @@
 #define _IGC_TSN_H_
 
 int igc_tsn_offload_apply(struct igc_adapter *adapter);
+int igc_tsn_reset(struct igc_adapter *adapter);
 
 #endif /* _IGC_BASE_H */

drivers/net/ethernet/intel/igc/igc.h
@@ -291,6 +291,8 @@ extern char igc_driver_name[];
 #define IGC_FLAG_RX_LEGACY		BIT(16)
 #define IGC_FLAG_TSN_QBV_ENABLED	BIT(17)
 
+#define IGC_FLAG_TSN_ANY_ENABLED	IGC_FLAG_TSN_QBV_ENABLED
+
 #define IGC_FLAG_RSS_FIELD_IPV4_UDP	BIT(6)
 #define IGC_FLAG_RSS_FIELD_IPV6_UDP	BIT(7)
 

intel/mainline-tracking

commit 9e8909cf079354efa560816cdc9b6a1711bfd979

igc: Fix data not cleared when all TSN offload are disabled
When all TSN features are disabled, only taprio related fields
were reset, other TSN features like preemption was not reset.

Fixing this by resetting all TSN features field when all TSN
features are disabeld.

Fixes: 61572d5f8f91 ("igc: Simplify TSN flags handling")
Signed-off-by: Faizal Rahim <faizal.abdul.rahim@linux.intel.com>

drivers/net/ethernet/intel/igc/igc_main.c
@@ -6350,9 +6350,51 @@ static int igc_qbv_clear_schedule(struct igc_adapter *adapter)
 	return 0;
 }
 
-static int igc_tsn_clear_schedule(struct igc_adapter *adapter)
+static void igc_qbu_clear_data(struct igc_adapter *adapter)
+{
+	int i;
+
+	for (i = 0; i < adapter->num_tx_queues; i++) {
+		struct igc_ring *ring = adapter->tx_ring[i];
+
+		ring->preemptible = false;
+	}
+
+	adapter->frame_preemption_active = false;
+}
+
+static void igc_cbs_clear_data(struct igc_adapter *adapter)
+{
+	int i;
+
+	for (i = 0; i < adapter->num_tx_queues; i++) {
+		struct igc_ring *ring = adapter->tx_ring[i];
+
+		ring->cbs_enable = false;
+		ring->idleslope = 0;
+		ring->sendslope = 0;
+		ring->hicredit = 0;
+		ring->locredit = 0;
+	}
+}
+
+static void igc_etf_clear_data(struct igc_adapter *adapter)
+{
+	int i;
+
+	for (i = 0; i < adapter->num_tx_queues; i++) {
+		struct igc_ring *ring = adapter->tx_ring[i];
+
+		ring->launchtime_enable = false;
+	}
+}
+
+int igc_tsn_clear_schedule(struct igc_adapter *adapter)
 {
 	igc_qbv_clear_schedule(adapter);
+	igc_qbu_clear_data(adapter);
+	igc_cbs_clear_data(adapter);
+	igc_etf_clear_data(adapter);
 
 	return 0;
 }
@@ -6543,7 +6585,7 @@ static int igc_tsn_enable_qbv_scheduling(struct igc_adapter *adapter,
 		err = igc_save_qbv_schedule(adapter, qopt);
 		break;
 	case TAPRIO_CMD_DESTROY:
-		err = igc_tsn_clear_schedule(adapter);
+		err = igc_qbv_clear_schedule(adapter);
 		break;
 	default:
 		return -EOPNOTSUPP;

drivers/net/ethernet/intel/igc/igc_tsn.c
@@ -340,8 +340,11 @@ int igc_tsn_reset(struct igc_adapter *adapter)
 
 	new_flags = igc_tsn_new_flags(adapter);
 
-	if (!(new_flags & IGC_FLAG_TSN_ANY_ENABLED))
-		return igc_tsn_disable_offload(adapter);
+	if (!(new_flags & IGC_FLAG_TSN_ANY_ENABLED)) {
+		igc_tsn_clear_schedule(adapter);
+		igc_tsn_disable_offload(adapter);
+		return 0;
+	}
 
 	err = igc_tsn_enable_offload(adapter);
 	if (err < 0)

drivers/net/ethernet/intel/igc/igc_tsn.h
@@ -7,5 +7,6 @@
 int igc_tsn_offload_apply(struct igc_adapter *adapter);
 int igc_tsn_reset(struct igc_adapter *adapter);
 void igc_tsn_adjust_txtime_offset(struct igc_adapter *adapter);
+int igc_tsn_clear_schedule(struct igc_adapter *adapter);
 
 #endif /* _IGC_BASE_H */