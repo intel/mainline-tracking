831d7d435cfbc107b3ce8627bf53f101be6518a8
Dapeng Mi <dapeng1.mi@linux.intel.com>
2024-11-14 05:36:55 +0000

    perf/x86/intel: Setup PEBS constraints base on counter & pdist map
    arch-PEBS provides CPUIDs to enumerate which counters support PEBS
    sampling and precise distribution PEBS sampling. Thus PEBS constraints
    can be dynamically configured base on these counter and precise
    distribution bitmap instead of defining them statically.
    
    Signed-off-by: Dapeng Mi <dapeng1.mi@linux.intel.com>

--- arch/x86/events/intel/ds.c
+++ arch/x86/events/intel/ds.c
@@ -2957,6 +2957,7 @@ static void __init intel_arch_pebs_init(void)
 	x86_pmu.pebs_buffer_size = PEBS_BUFFER_SIZE;
 	x86_pmu.drain_pebs = intel_pmu_drain_arch_pebs;
 	x86_pmu.pebs_capable = ~0ULL;
+	x86_pmu.flags |= PMU_FL_PEBS_ALL;
 }
 
 /*
