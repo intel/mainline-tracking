acc44baa392a9f8bee82dcac03e5ad7a843756bd
Dapeng Mi <dapeng1.mi@linux.intel.com>
2024-11-25 09:05:25 +0000

    perf/core: Support to capture higher width vector registers
    Arch-PEBS supports to capture more vector registers like OPMASK/YMM/ZMM
    registers besides XMM registers. This patch extends PERF_SAMPLE_REGS_INTR
    and PERF_SAMPLE_REGS_USER attributes to support these new vector registers
    capturing at interrupt and user space.
    
    The arrays sample_regs_intr/user__ext[] is added into perf_event_attr
    structure to record user configured extended register bitmap and a helper
    perf_reg_ext_validate() is added to validate if these registers are
    supported on some specific PMUs. Furthermore considering to leave enough
    space to support more GPRs like R16 ~ R31 introduced by APX in the future,
    directly extend the array size to 7.
    
    This patch just adds the common perf/core support, the x86/intel specific
    support would be added in next patch.
    
    Co-developed-by: Kan Liang <kan.liang@linux.intel.com>
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>
    Signed-off-by: Dapeng Mi <dapeng1.mi@linux.intel.com>

--- arch/powerpc/perf/perf_regs.c
+++ arch/powerpc/perf/perf_regs.c
@@ -132,6 +132,11 @@ int perf_reg_validate(u64 mask)
 	return 0;
 }
 
+int perf_reg_ext_validate(unsigned long *mask, unsigned int size)
+{
+	return -EINVAL;
+}
+
 u64 perf_reg_abi(struct task_struct *task)
 {
 	if (is_tsk_32bit_task(task))
