453494136835fd98fd7f375f04c2e25161602de2
Jani Nikula <jani.nikula@intel.com>
2025-01-09 12:50:32 +0200

    drm/xe: remove unused xe_pciids.h harder, add missing PCI ID
    Commit 493454445c95 ("drm/xe: switch to common PCI ID macros") removed
    xe_pciids.h via drm-intel-next. In the mean time, commit ae78ec0a52c4
    ("drm/xe/ptl: Add another PTL PCI ID") added to xe_pciids.h via
    drm-xe-next.
    
    The two commits were merged in commit 8f109f287fdc ("Merge drm/drm-next
    into drm-xe-next"), but xe_pciids.h wasn't removed, and the PCI ID
    wasn't added to pciids.h.
    
    Remove xe_pciids.h, and add the PCI ID to pciids.h.
    
    Cc: Matt Atwood <matthew.s.atwood@intel.com>
    Cc: Matt Roper <matthew.d.roper@intel.com>
    Cc: Rodrigo Vivi <rodrigo.vivi@intel.com>
    Fixes: 8f109f287fdc ("Merge drm/drm-next into drm-xe-next")
    Reported-by: Stephen Rothwell <sfr@canb.auug.org.au>
    Closes: https://lore.kernel.org/r/20241125120921.1bbc1930@canb.auug.org.au
    Reviewed-by: Tejas Upadhyay <tejas.upadhyay@intel.com>
    Reviewed-by: Matt Roper <matthew.d.roper@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20250109105032.2585416-1-jani.nikula@intel.com
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    (cherry picked from commit 6a04bb5a2046067681257d5dd69a724856c8fbcb)

--- include/drm/intel/pciids.h
+++ include/drm/intel/pciids.h
@@ -863,6 +863,8 @@
 	MACRO__(0xB08F, ## __VA_ARGS__), \
 	MACRO__(0xB090, ## __VA_ARGS__), \
 	MACRO__(0xB0A0, ## __VA_ARGS__), \
+	MACRO__(0xB0A1, ## __VA_ARGS__), \
+	MACRO__(0xB0A2, ## __VA_ARGS__), \
 	MACRO__(0xB0B0, ## __VA_ARGS__)
 
 #endif /* __PCIIDS_H__ */
