6bb11861ff7736b5bbeceb034592953223e409e1
Kan Liang <kan.liang@linux.intel.com>
2025-03-24 17:30:44 +0000

    perf: Add a EVENT_GUEST flag
    Current perf doesn't explicitly schedule out all exclude_guest events
    while the guest is running. There is no problem with the current
    emulated vPMU. Because perf owns all the PMU counters. It can mask the
    counter which is assigned to an exclude_guest event when a guest is
    running (Intel way), or set the corresponding HOSTONLY bit in evsentsel
    (AMD way). The counter doesn't count when a guest is running.
    
    However, either way doesn't work with the introduced mediated vPMU.
    A guest owns all the PMU counters when it's running. The host should not
    mask any counters. The counter may be used by the guest. The evsentsel
    may be overwritten.
    
    Perf should explicitly schedule out all exclude_guest events to release
    the PMU resources when entering a guest, and resume the counting when
    exiting the guest.
    
    It's possible that an exclude_guest event is created when a guest is
    running. The new event should not be scheduled in as well.
    
    The ctx time is shared among different PMUs. The time cannot be stopped
    when a guest is running. It is required to calculate the time for events
    from other PMUs, e.g., uncore events. Add timeguest to track the guest
    run time. For an exclude_guest event, the elapsed time equals
    the ctx time - guest time.
    Cgroup has dedicated times. Use the same method to deduct the guest time
    from the cgroup time as well.
    
    Co-developed-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>
    Signed-off-by: Mingwei Zhang <mizhang@google.com>
    [sean: massage comments]
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- include/linux/perf_event.h
+++ include/linux/perf_event.h
@@ -1046,6 +1046,11 @@ struct perf_event_context {
 	struct perf_time_ctx		time;
 
 	/*
+	 * Context clock, runs when in the guest mode.
+	 */
+	struct perf_time_ctx		timeguest;
+
+	/*
 	 * These fields let us detect when two contexts have both
 	 * been cloned (inherited) from a common ancestor.
 	 */
@@ -1177,6 +1182,7 @@ struct bpf_perf_event_data_kern {
  */
 struct perf_cgroup_info {
 	struct perf_time_ctx		time;
+	struct perf_time_ctx		timeguest;
 	int				active;
 };
 
