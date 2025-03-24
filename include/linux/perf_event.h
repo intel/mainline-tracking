7c02d8323a1bba5a170efd22c772fd9515c18484
Kan Liang <kan.liang@linux.intel.com>
2025-03-24 17:30:43 +0000

    perf: Clean up perf ctx time
    The current perf tracks two timestamps for the normal ctx and cgroup.
    The same type of variables and similar codes are used to track the
    timestamps. In the following patch, the third timestamp to track the
    guest time will be introduced.
    To avoid the code duplication, add a new struct perf_time_ctx and factor
    out a generic function update_perf_time_ctx().
    
    No functional change.
    
    Suggested-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>
    Signed-off-by: Mingwei Zhang <mizhang@google.com>
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- include/linux/perf_event.h
+++ include/linux/perf_event.h
@@ -1000,6 +1000,11 @@ struct perf_event_groups {
 	u64				index;
 };
 
+struct perf_time_ctx {
+	u64		time;
+	u64		stamp;
+	u64		offset;
+};
 
 /**
  * struct perf_event_context - event context structure
@@ -1038,9 +1043,7 @@ struct perf_event_context {
 	/*
 	 * Context clock, runs when context enabled.
 	 */
-	u64				time;
-	u64				timestamp;
-	u64				timeoffset;
+	struct perf_time_ctx		time;
 
 	/*
 	 * These fields let us detect when two contexts have both
@@ -1173,9 +1176,7 @@ struct bpf_perf_event_data_kern {
  * This is a per-cpu dynamically allocated data structure.
  */
 struct perf_cgroup_info {
-	u64				time;
-	u64				timestamp;
-	u64				timeoffset;
+	struct perf_time_ctx		time;
 	int				active;
 };
 
