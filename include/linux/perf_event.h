4cdfd036c183754b28cc9a0e9bdae165ebbfefcb
Kan Liang <kan.liang@linux.intel.com>
2025-05-13 10:23:53 -0700

    perf: Add APIs to load/put guest mediated PMU context
    Add exported APIs to load/put a guest mediated PMU context.  KVM will
    load the guest PMU shortly before VM-Enter, and put the guest PMU shortly
    after VM-Exit.
    
    On the perf side of things, schedule out all exclude_guest events when the
    guest context is loaded, and schedule them back in when the guest context
    is put.  I.e. yield the hardware PMU resources to the guest, by way of KVM.
    
    Note, perf is only responsible for managing host context.  KVM is
    responsible for loading/storing guest state to/from hardware.
    
    Suggested-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>
    Signed-off-by: Mingwei Zhang <mizhang@google.com>
    [sean: shuffle patches around, write changelog]
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- include/linux/perf_event.h
+++ include/linux/perf_event.h
@@ -1929,6 +1929,8 @@ extern u64 perf_event_pause(struct perf_event *event, bool reset);
 #ifdef CONFIG_PERF_GUEST_MEDIATED_PMU
 int perf_create_mediated_pmu(void);
 void perf_release_mediated_pmu(void);
+void perf_load_guest_context(unsigned long data);
+void perf_put_guest_context(void);
 #endif
 
 #else /* !CONFIG_PERF_EVENTS: */
