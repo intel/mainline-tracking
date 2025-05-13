6944d647d529abbf90154923f9f579ccb55d0c28
Sean Christopherson <seanjc@google.com>
2025-05-13 10:01:17 -0700

    perf: Move security_perf_event_free() call to __free_event()
    Move the freeing of any security state associated with a perf event from
    _free_event() to __free_event(), i.e. invoke security_perf_event_free() in
    the error paths for perf_event_alloc().  This will allow adding potential
    error paths in perf_event_alloc() that can occur after allocating security
    state.
    
    Note, kfree() and thus security_perf_event_free() is a nop if
    event->security is NULL, i.e. calling security_perf_event_free() even if
    security_perf_event_alloc() fails or is never reached is functionality ok.
    
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -5602,6 +5602,8 @@ static void __free_event(struct perf_event *event)
 {
 	struct pmu *pmu = event->pmu;
 
+	security_perf_event_free(event);
+
 	if (event->attach_state & PERF_ATTACH_CALLCHAIN)
 		put_callchain_buffers();
 
@@ -5664,8 +5666,6 @@ static void _free_event(struct perf_event *event)
 	irq_work_sync(&event->pending_disable_irq);
 
 	unaccount_event(event);
-
-	security_perf_event_free(event);
 
 	if (event->rb) {
 		/*
