11f686c7ebd18a4d5877ceda5806c23f454a6678
Sean Christopherson <seanjc@google.com>
2025-05-13 17:46:44 -0700

    KVM: Add a simplified wrapper for registering perf callbacks
    Add a parameter-less API for registering perf callbacks in anticipation of
    introducing another x86-only parameter for handling mediated PMU PMIs.
    
    No functional change intended.
    
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- arch/riscv/kvm/main.c
+++ arch/riscv/kvm/main.c
@@ -159,7 +159,7 @@ static int __init riscv_kvm_init(void)
 		kvm_info("AIA available with %d guest external interrupts\n",
 			 kvm_riscv_aia_nr_hgei);
 
-	kvm_register_perf_callbacks(NULL);
+	kvm_register_perf_callbacks();
 
 	rc = kvm_init(sizeof(struct kvm_vcpu), 0, THIS_MODULE);
 	if (rc) {
