374f0a1283e31f3db048fda7bd35cb0b9652789a
Wong, Chee Yin <chee.yin.wong@intel.com>
2025-06-30 13:35:11 +0800

    Update PTL GuC version major naming and HuC version
    Signed-off-by: Wong, Chee Yin <chee.yin.wong@intel.com>

--- drivers/gpu/drm/xe/xe_uc_fw.c
+++ drivers/gpu/drm/xe/xe_uc_fw.c
@@ -107,7 +107,7 @@ struct fw_blobs_by_type {
 };
 
 #define XE_GUC_FIRMWARE_DEFS(fw_def, mmp_ver, major_ver)			\
-	fw_def(PANTHERLAKE,	mmp_ver(xe,	guc,	ptl,	70, 47, 0))	\
+	fw_def(PANTHERLAKE,	major_ver(xe,	guc,	ptl,	70, 47, 0))	\
 	fw_def(BATTLEMAGE,	major_ver(xe,	guc,	bmg,	70, 44, 1))	\
 	fw_def(LUNARLAKE,	major_ver(xe,	guc,	lnl,	70, 29, 2))	\
 	fw_def(METEORLAKE,	major_ver(i915,	guc,	mtl,	70, 29, 2))	\
@@ -121,7 +121,7 @@ struct fw_blobs_by_type {
 	fw_def(TIGERLAKE,	major_ver(i915,	guc,	tgl,	70, 29, 2))
 
 #define XE_HUC_FIRMWARE_DEFS(fw_def, mmp_ver, no_ver)		\
-	fw_def(PANTHERLAKE,	mmp_ver(xe,	huc,		ptl, 10, 3, 2))	\
+	fw_def(PANTHERLAKE,	no_ver(xe,	huc,		ptl))	        \
 	fw_def(BATTLEMAGE,	no_ver(xe,	huc,		bmg))		\
 	fw_def(LUNARLAKE,	no_ver(xe,	huc,		lnl))		\
 	fw_def(METEORLAKE,	no_ver(i915,	huc_gsc,	mtl))		\
