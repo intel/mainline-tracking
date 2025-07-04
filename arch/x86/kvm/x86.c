2ee63e38a5bc5418e85af793c33ff0cb36d8b546
Yang Weijiang <weijiang.yang@intel.com>
2025-07-04 01:49:39 -0700

    KVM: x86: Initialize kvm_caps.supported_xss
    Set original kvm_caps.supported_xss to (host_xss & KVM_SUPPORTED_XSS) if
    XSAVES is supported. host_xss contains the host supported xstate feature
    bits for thread FPU context switch, KVM_SUPPORTED_XSS includes all KVM
    enabled XSS feature bits, the resulting value represents the supervisor
    xstates that are available to guest and are backed by host FPU framework
    for swapping {guest,host} XSAVE-managed registers/MSRs.
    
    Signed-off-by: Yang Weijiang <weijiang.yang@intel.com>
    Reviewed-by: Maxim Levitsky <mlevitsk@redhat.com>
    Reviewed-by: Chao Gao <chao.gao@intel.com>
    Tested-by: Mathias Krause <minipli@grsecurity.net>
    Tested-by: John Allen <john.allen@amd.com>
    Signed-off-by: Chao Gao <chao.gao@intel.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -224,6 +224,8 @@ static struct kvm_user_return_msrs __percpu *user_return_msrs;
 				| XFEATURE_MASK_BNDCSR | XFEATURE_MASK_AVX512 \
 				| XFEATURE_MASK_PKRU | XFEATURE_MASK_XTILE)
 
+#define KVM_SUPPORTED_XSS     0
+
 bool __read_mostly allow_smaller_maxphyaddr = 0;
 EXPORT_SYMBOL_GPL(allow_smaller_maxphyaddr);
 
@@ -9744,13 +9746,16 @@ int kvm_x86_vendor_init(struct kvm_x86_init_ops *ops)
 		kvm_host.xcr0 = xgetbv(XCR_XFEATURE_ENABLED_MASK);
 		kvm_caps.supported_xcr0 = kvm_host.xcr0 & KVM_SUPPORTED_XCR0;
 	}
+
+	if (boot_cpu_has(X86_FEATURE_XSAVES)) {
+		rdmsrq(MSR_IA32_XSS, kvm_host.xss);
+		kvm_caps.supported_xss = kvm_host.xss & KVM_SUPPORTED_XSS;
+	}
+
 	kvm_caps.supported_quirks = KVM_X86_VALID_QUIRKS;
 	kvm_caps.inapplicable_quirks = KVM_X86_CONDITIONAL_QUIRKS;
 
 	rdmsrq_safe(MSR_EFER, &kvm_host.efer);
-
-	if (boot_cpu_has(X86_FEATURE_XSAVES))
-		rdmsrq(MSR_IA32_XSS, kvm_host.xss);
 
 	kvm_init_pmu_capability(ops->pmu_ops);
 
