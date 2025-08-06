6d26ca28c7b910f8cd0bc802135a2a00e9d82fe4
Dapeng Mi <dapeng1.mi@linux.intel.com>
2025-08-06 12:56:59 -0700

    KVM: x86/pmu: Load/put mediated PMU context when entering/exiting guest
    Implement the PMU "world switch" between host perf and guest mediated PMU.
    When loading guest state, call into perf to switch from host to guest, and
    then load guest state into hardware, and then reverse those actions when
    putting guest state.
    
    On the KVM side, when loading guest state, zero PERF_GLOBAL_CTRL to ensure
    all counters are disabled, then load selectors and counters, and finally
    call into vendor code to load control/status information.  While VMX and
    SVM use different mechanisms to avoid counting host activity while guest
    controls are loaded, both implementations require PERF_GLOBAL_CTRL to be
    zeroed when the event selectors are in flux.
    
    When putting guest state, reverse the order, and save and zero controls
    and status prior to saving+zeroing selectors and counters.  Defer clearing
    PERF_GLOBAL_CTRL to vendor code, as only SVM needs to manually clear the
    MSR; VMX configures PERF_GLOBAL_CTRL to be atomically cleared by the CPU
    on VM-Exit.
    
    Handle the difference in MSR layouts between Intel and AMD by communicating
    the bases and stride via kvm_pmu_ops.  Because KVM requires Intel v4 (and
    full-width writes) and AMD v2, the MSRs to load/save are constant for a
    given vendor, i.e. do not vary based on the guest PMU, and do not vary
    based on host PMU (because KVM will simply disable mediated PMU support if
    the necessary MSRs are unsupported).
    
    Except for retrieving the guest's PERF_GLOBAL_CTRL, which needs to be read
    before invoking any fastpath handler (spoiler alert), perform the context
    switch around KVM's inner run loop.  State only needs to be synchronized
    from hardware before KVM can access the software "caches".
    
    Note, VMX already grabs the guest's PERF_GLOBAL_CTRL immediately after
    VM-Exit, as hardware saves value into the VMCS.
    
    Co-developed-by: Mingwei Zhang <mizhang@google.com>
    Signed-off-by: Mingwei Zhang <mizhang@google.com>
    Co-developed-by: Sandipan Das <sandipan.das@amd.com>
    Signed-off-by: Sandipan Das <sandipan.das@amd.com>
    Signed-off-by: Dapeng Mi <dapeng1.mi@linux.intel.com>
    Tested-by: Xudong Hao <xudong.hao@intel.com>
    Co-developed-by: Sean Christopherson <seanjc@google.com>
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -11341,6 +11341,8 @@ static int vcpu_enter_guest(struct kvm_vcpu *vcpu)
 		run_flags |= KVM_RUN_LOAD_DEBUGCTL;
 	vcpu->arch.host_debugctl = debug_ctl;
 
+	kvm_mediated_pmu_load(vcpu);
+
 	guest_timing_enter_irqoff();
 
 	/*
@@ -11378,6 +11380,8 @@ static int vcpu_enter_guest(struct kvm_vcpu *vcpu)
 	}
 
 	kvm_load_host_pkru(vcpu);
+
+	kvm_mediated_pmu_put(vcpu);
 
 	/*
 	 * Do this here before restoring debug registers on the host.  And
