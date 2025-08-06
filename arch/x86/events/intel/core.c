ec9597079f0b9ee03e6fddf6aef4655d6fb214df
Kan Liang <kan.liang@linux.intel.com>
2025-08-06 12:56:34 -0700

    perf/x86/intel: Support PERF_PMU_CAP_MEDIATED_VPMU
    Apply the PERF_PMU_CAP_MEDIATED_VPMU for Intel core PMU. It only indicates
    that the perf side of core PMU is ready to support the mediated vPMU.
    Besides the capability, the hypervisor, a.k.a. KVM, still needs to check
    the PMU version and other PMU features/capabilities to decide whether to
    enable support mediated vPMUs.
    
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>
    Signed-off-by: Mingwei Zhang <mizhang@google.com>
    [sean: massage changelog]
    Tested-by: Xudong Hao <xudong.hao@intel.com>
    Signed-off-by: Sean Christopherson <seanjc@google.com>

--- arch/x86/events/intel/core.c
+++ arch/x86/events/intel/core.c
@@ -6013,6 +6013,8 @@ static void intel_pmu_check_hybrid_pmus(struct x86_hybrid_pmu *pmu)
 
 	intel_pmu_check_event_constraints_all(&pmu->pmu);
 
+	pmu->pmu.capabilities |= PERF_PMU_CAP_MEDIATED_VPMU;
+
 	intel_pmu_check_extra_regs(pmu->extra_regs);
 }
 
@@ -7643,6 +7645,9 @@ __init int intel_pmu_init(void)
 		if (x86_pmu.intel_cap.anythread_deprecated)
 			pr_cont(" AnyThread deprecated, ");
 	}
+
+	/* The perf side of core PMU is ready to support the mediated vPMU. */
+	x86_get_pmu(smp_processor_id())->capabilities |= PERF_PMU_CAP_MEDIATED_VPMU;
 
 	/*
 	 * Many features on and after V6 require dynamic constraint,
