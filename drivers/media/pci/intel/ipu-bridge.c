cff0a1ac43aa4e4aab73d712a38311789db15a9e
linya14x <linx.yang@intel.com>
2025-09-06 17:14:52 +0800

    media: pci: enable lt6911gxd in ipu-bridge
    Signed-off-by: linya14x <linx.yang@intel.com>

--- drivers/media/pci/intel/ipu-bridge.c
+++ drivers/media/pci/intel/ipu-bridge.c
@@ -92,6 +92,8 @@ static const struct ipu_sensor_config ipu_supported_sensors[] = {
 	IPU_SENSOR_CONFIG("OVTI8856", 3, 180000000, 360000000, 720000000),
 	/* Toshiba T4KA3 */
 	IPU_SENSOR_CONFIG("XMCC0003", 1, 321468000),
+	/* Lontium lt6911gxd */
+	IPU_SENSOR_CONFIG("INTC1124", 0),
 };
 
 static const struct ipu_property_names prop_names = {
