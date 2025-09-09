bd9f2f86060893bccdc6bef8910f06a0ff0ee054
Kan Liang <kan.liang@linux.intel.com>
2025-09-09 14:04:50 -0700

    perf/x86: Enable YMM sampling using sample_simd_vec_reg_* fields
    This patch introduces support for sampling YMM registers via the
    sample_simd_vec_reg_* fields.
    
    Each YMM register consists of 4 u64 words, assembled from two halves:
    XMM (the lower 2 u64 words) and YMMH (the upper 2 u64 words). Although
    both XMM and YMMH data can be retrieved with a single xsaves instruction,
    they are stored in separate locations. The perf_simd_reg_value() function
    is responsible for assembling these halves into a complete YMM register
    for output to userspace.
    
    Additionally, sample_simd_vec_reg_qwords should be set to 4 to indicate
    YMM sampling.
    
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>
    Co-developed-by: Dapeng Mi <dapeng1.mi@linux.intel.com>
    Signed-off-by: Dapeng Mi <dapeng1.mi@linux.intel.com>

--- arch/x86/events/perf_event.h
+++ arch/x86/events/perf_event.h
@@ -145,6 +145,15 @@ static inline bool event_needs_xmm(struct perf_event *event)
 	return false;
 }
 
+static inline bool event_needs_ymm(struct perf_event *event)
+{
+	if (event->attr.sample_simd_regs_enabled &&
+	    event->attr.sample_simd_vec_reg_qwords >= PERF_X86_YMM_QWORDS)
+		return true;
+
+	return false;
+}
+
 struct amd_nb {
 	int nb_id;  /* NorthBridge id */
 	int refcnt; /* reference count */
