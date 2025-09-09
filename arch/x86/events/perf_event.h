88dda5f24626451d9b7d74e8ff893e2af33a0c81
Kan Liang <kan.liang@linux.intel.com>
2025-09-09 14:04:49 -0700

    perf/x86: Move XMM to sample_simd_vec_regs
    The XMM0-15 are SIMD registers. Move them from sample_regs to
    sample_simd_vec_regs. Reject access to the extended space of the sample_regs
    if the new sample_simd_vec_regs is used.
    
    The perf_reg_value requires the abi to understand the layout of the
    sample_regs. Add the abi information in the struct x86_perf_regs.
    
    Implement the X86-specific perf_simd_reg_validate to validate the SIMD
    registers configuration from the user tool. Only the XMM0-15 is
    supported now. More registers will be added in the following patches.
    Implement the X86-specific perf_simd_reg_value to retrieve the XMM
    value.
    
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>
    Co-developed-by: Dapeng Mi <dapeng1.mi@linux.intel.com>
    Signed-off-by: Dapeng Mi <dapeng1.mi@linux.intel.com>

--- arch/x86/events/perf_event.h
+++ arch/x86/events/perf_event.h
@@ -133,6 +133,18 @@ static inline bool is_acr_event_group(struct perf_event *event)
 	return check_leader_group(event->group_leader, PERF_X86_EVENT_ACR);
 }
 
+static inline bool event_needs_xmm(struct perf_event *event)
+{
+	if (event->attr.sample_simd_regs_enabled &&
+	    event->attr.sample_simd_vec_reg_qwords >= PERF_X86_XMM_QWORDS)
+		return true;
+
+	if (!event->attr.sample_simd_regs_enabled &&
+	    event_has_extended_regs(event))
+		return true;
+	return false;
+}
+
 struct amd_nb {
 	int nb_id;  /* NorthBridge id */
 	int refcnt; /* reference count */
