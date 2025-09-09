bddee1cc5bd2bf094b5349b7b5154aa87247a5f7
Kan Liang <kan.liang@linux.intel.com>
2025-09-09 14:04:51 -0700

    perf/x86: Enable ZMM sampling using sample_simd_vec_reg_* fields
    This patch adds support for sampling ZMM registers via the
    sample_simd_vec_reg_* fields.
    
    Each ZMM register consists of 8 u64 words. Current x86 hardware supports
    up to 32 ZMM registers. For ZMM registers from ZMM0 to ZMM15, they are
    assembled from three parts: XMM (the lower 2 u64 words),
    YMMH (the middle 2 u64 words), and ZMMH (the upper 4 u64 words). The
    perf_simd_reg_value() function is responsible for assembling these three
    parts into a complete ZMM register for output to userspace.
    
    For ZMM registers ZMM16 to ZMM31, each register can be read as a whole
    and directly outputted to userspace.
    
    Additionally, sample_simd_vec_reg_qwords should be set to 8 to indicate
    ZMM sampling.
    
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>
    Co-developed-by: Dapeng Mi <dapeng1.mi@linux.intel.com>
    Signed-off-by: Dapeng Mi <dapeng1.mi@linux.intel.com>

--- arch/x86/kernel/perf_regs.c
+++ arch/x86/kernel/perf_regs.c
@@ -78,6 +78,7 @@ u64 perf_reg_value(struct pt_regs *regs, int idx)
 }
 
 #define PERF_X86_YMMH_QWORDS	(PERF_X86_YMM_QWORDS / 2)
+#define PERF_X86_ZMMH_QWORDS	(PERF_X86_ZMM_QWORDS / 2)
 
 u64 perf_simd_reg_value(struct pt_regs *regs, int idx,
 			u16 qwords_idx, bool pred)
@@ -92,6 +93,13 @@ u64 perf_simd_reg_value(struct pt_regs *regs, int idx,
 			 qwords_idx >= PERF_X86_SIMD_QWORDS_MAX))
 		return 0;
 
+	if (idx >= PERF_X86_H16ZMM_BASE) {
+		if (!perf_regs->h16zmm_regs)
+			return 0;
+		return perf_regs->h16zmm_regs[(idx - PERF_X86_H16ZMM_BASE) *
+					PERF_X86_ZMM_QWORDS + qwords_idx];
+	}
+
 	if (qwords_idx < PERF_X86_XMM_QWORDS) {
 		if (!perf_regs->xmm_regs)
 			return 0;
@@ -102,6 +110,11 @@ u64 perf_simd_reg_value(struct pt_regs *regs, int idx,
 			return 0;
 		return perf_regs->ymmh_regs[idx * PERF_X86_YMMH_QWORDS +
 					    qwords_idx - PERF_X86_XMM_QWORDS];
+	} else if (qwords_idx < PERF_X86_ZMM_QWORDS) {
+		if (!perf_regs->zmmh_regs)
+			return 0;
+		return perf_regs->zmmh_regs[idx * PERF_X86_ZMMH_QWORDS +
+					    qwords_idx - PERF_X86_YMM_QWORDS];
 	}
 
 	return 0;
@@ -119,7 +132,8 @@ int perf_simd_reg_validate(u16 vec_qwords, u64 vec_mask,
 			return -EINVAL;
 	} else {
 		if (vec_qwords != PERF_X86_XMM_QWORDS &&
-		    vec_qwords != PERF_X86_YMM_QWORDS)
+		    vec_qwords != PERF_X86_YMM_QWORDS &&
+		    vec_qwords != PERF_X86_ZMM_QWORDS)
 			return -EINVAL;
 		if (vec_mask & ~PERF_X86_SIMD_VEC_MASK)
 			return -EINVAL;
