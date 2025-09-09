9d92f01427f74fa70c043c65601f43054f071e24
Kan Liang <kan.liang@linux.intel.com>
2025-09-09 14:04:54 -0700

    perf/x86: Enable SSP sampling using sample_regs_* fields
    This patch enables sampling of CET SSP register via the sample_regs_*
    fields.
    
    To sample SSP, the sample_simd_regs_enabled field must be set. This
    allows the spare space (reclaimed from the original XMM space) in the
    sample_regs_* fields to be used for representing SSP.
    
    Similar with eGPRs sampling, the perf_reg_value() function needs to
    check if the PERF_SAMPLE_REGS_ABI_SIMD flag is set first, and then
    determine whether to output SSP or legacy XMM registers to userspace.
    
    Additionally, arch-PEBS supports sampling SSP, which is placed into the
    GPRs group. This patch also enables arch-PEBS-based SSP sampling.
    
    Currently, SSP sampling is only supported on the x86_64 architecture, as
    CET is only available on x86_64 platforms.
    
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>
    Co-developed-by: Dapeng Mi <dapeng1.mi@linux.intel.com>
    Signed-off-by: Dapeng Mi <dapeng1.mi@linux.intel.com>

--- arch/x86/kernel/perf_regs.c
+++ arch/x86/kernel/perf_regs.c
@@ -70,6 +70,11 @@ u64 perf_reg_value(struct pt_regs *regs, int idx)
 					return 0;
 				return perf_regs->egpr_regs[idx - PERF_REG_X86_R16];
 			}
+			if (idx == PERF_REG_X86_SSP) {
+				if (!perf_regs->cet_regs)
+					return 0;
+				return perf_regs->cet_regs[1];
+			}
 		} else {
 			if (idx >= PERF_REG_X86_XMM0 && idx < PERF_REG_X86_XMM_MAX) {
 				if (!perf_regs->xmm_regs)
