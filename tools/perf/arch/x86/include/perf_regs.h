b619322f0399f5d40f15102c9994fa451424bb79
Kan Liang <kan.liang@linux.intel.com>
2025-09-09 14:04:56 -0700

    perf tools: Only support legacy regs for the PT and PERF_REGS_MASK
    The PERF_REG_X86_64_MAX is going to be updated to support more regs,
    e.g., eGPRs.
    However, the PT and PERF_REGS_MASK will not be touched in the POC.
    Using the PERF_REG_X86_R15 + 1 to replace PERF_REG_X86_64_MAX.
    
    Acked-by: Adrian Hunter <adrian.hunter@intel.com>
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>
    Signed-off-by: Dapeng Mi <dapeng1.mi@linux.intel.com>

--- tools/perf/arch/x86/include/perf_regs.h
+++ tools/perf/arch/x86/include/perf_regs.h
@@ -17,7 +17,7 @@ void perf_regs_load(u64 *regs);
 		       (1ULL << PERF_REG_X86_ES) | \
 		       (1ULL << PERF_REG_X86_FS) | \
 		       (1ULL << PERF_REG_X86_GS))
-#define PERF_REGS_MASK (((1ULL << PERF_REG_X86_64_MAX) - 1) & ~REG_NOSUPPORT)
+#define PERF_REGS_MASK (((1ULL << (PERF_REG_X86_R15 + 1)) - 1) & ~REG_NOSUPPORT)
 #define PERF_SAMPLE_REGS_ABI PERF_SAMPLE_REGS_ABI_64
 #endif
 
