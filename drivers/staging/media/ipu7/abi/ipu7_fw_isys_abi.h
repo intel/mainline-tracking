6da5d9964d3764352d9d8d6a4e22bca2b0ad5e72
linya14x <linx.yang@intel.com>
2025-11-14 18:41:08 +0800

    staging: ipu7: Update IPU7 firmware ABI version to 1.2.1.20251215_224531
    Signed-off-by: Hao Yao <hao.yao@intel.com>
    Signed-off-by: linya14x <linx.yang@intel.com>

--- drivers/staging/media/ipu7/abi/ipu7_fw_isys_abi.h
+++ drivers/staging/media/ipu7/abi/ipu7_fw_isys_abi.h
@@ -47,7 +47,6 @@ enum ipu7_insys_resp_type {
 	IPU_INSYS_RESP_TYPE_FRAME_EOF = 8,
 	IPU_INSYS_RESP_TYPE_STREAM_START_AND_CAPTURE_DONE = 9,
 	IPU_INSYS_RESP_TYPE_STREAM_CAPTURE_DONE = 10,
-	IPU_INSYS_RESP_TYPE_PWM_IRQ = 11,
 	N_IPU_INSYS_RESP_TYPE
 };
 
@@ -201,7 +200,8 @@ enum ipu7_insys_mipi_dt_rename_mode {
 enum ipu7_insys_output_link_dest {
 	IPU_INSYS_OUTPUT_LINK_DEST_MEM = 0,
 	IPU_INSYS_OUTPUT_LINK_DEST_PSYS = 1,
-	IPU_INSYS_OUTPUT_LINK_DEST_IPU_EXTERNAL = 2
+	IPU_INSYS_OUTPUT_LINK_DEST_IPU_EXTERNAL = 2,
+	N_IPU_INSYS_OUTPUT_LINK_DEST
 };
 
 enum ipu7_insys_dpcm_type {
@@ -220,8 +220,11 @@ enum ipu7_insys_dpcm_predictor {
 
 enum ipu7_insys_send_queue_token_flag {
 	IPU_INSYS_SEND_QUEUE_TOKEN_FLAG_NONE = 0,
-	IPU_INSYS_SEND_QUEUE_TOKEN_FLAG_FLUSH_FORCE = 1
+	IPU_INSYS_SEND_QUEUE_TOKEN_FLAG_FLUSH_FORCE = 1,
+	N_IPU_INSYS_SEND_QUEUE_TOKEN_FLAG
 };
+
+#define IPU_INSYS_MIPI_FRAME_NUMBER_DONT_CARE UINT16_MAX
 
 #pragma pack(push, 1)
 struct ipu7_insys_resolution {
@@ -312,7 +315,7 @@ struct ipu7_insys_resp {
 	u8 pin_id;
 	u8 frame_id;
 	u8 skip_frame;
-	u8 pad[2];
+	u16 mipi_fn;
 };
 
 struct ipu7_insys_resp_queue_token {
