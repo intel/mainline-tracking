dec2b0235e0ecb01a309c4a713d8ee08cf9054d4
Brian Nguyen <brian3.nguyen@intel.com>
2025-12-13 05:32:34 +0800

    drm/xe: Append page reclamation action to tlb inval
    Add page reclamation action to tlb inval backend. The page reclamation
    action is paired with range tlb invalidations so both are issued at the
    same time.
    
    Page reclamation will issue the TLB invalidation with an invalid seqno
    and a H2G page reclamation action with the fence's corresponding seqno
    and handle the fence accordingly on page reclaim action done handler.
    
    If page reclamation fails, tlb timeout handler will be responsible for
    signalling fence and cleaning up.
    
    v2:
     - add send_page_reclaim to patch.
     - Remove flush_cache and use prl_sa pointer to determine PPC flush
       instead of explicit bool. Add NULL as fallback for others. (Matthew B)
    
    v3:
     - Add comments for flush_cache with media.
    
    Signed-off-by: Brian Nguyen <brian3.nguyen@intel.com>
    Suggested-by: Matthew Brost <matthew.brost@intel.com>
    Reviewed-by: Matthew Brost <matthew.brost@intel.com>

--- drivers/gpu/drm/xe/xe_vm.c
+++ drivers/gpu/drm/xe/xe_vm.c
@@ -3918,7 +3918,7 @@ int xe_vm_range_tilemask_tlb_inval(struct xe_vm *vm, u64 start,
 
 		err = xe_tlb_inval_range(&tile->primary_gt->tlb_inval,
 					 &fence[fence_id], start, end,
-					 vm->usm.asid);
+					 vm->usm.asid, NULL);
 		if (err)
 			goto wait;
 		++fence_id;
@@ -3931,7 +3931,7 @@ int xe_vm_range_tilemask_tlb_inval(struct xe_vm *vm, u64 start,
 
 		err = xe_tlb_inval_range(&tile->media_gt->tlb_inval,
 					 &fence[fence_id], start, end,
-					 vm->usm.asid);
+					 vm->usm.asid, NULL);
 		if (err)
 			goto wait;
 		++fence_id;
