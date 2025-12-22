03bd7cbfbac297f0542f072f067b89776888a7ba
Gustavo Sousa <gustavo.sousa@intel.com>
2025-12-22 19:18:47 -0300

    drm/i915/display_wa: Keep enum intel_display_wa sorted
    For a consistent way of updating enum intel_display_wa, let's sort it by
    lineage number and add a comment asking for future updates to keep it
    sorted.
    
    In the same way, let's also keep __intel_display_wa() sorted.
    
    --
    
    Signed-off-by: Gustavo Sousa <gustavo.sousa@intel.com>
    Note: this can be easily reviewed with `git show --color-moved`.
    Reviewed-by: Matt Roper <matthew.d.roper@intel.com>

--- drivers/gpu/drm/i915/display/intel_display_wa.c
+++ drivers/gpu/drm/i915/display/intel_display_wa.c
@@ -62,9 +62,6 @@ static bool intel_display_needs_wa_16025573575(struct intel_display *display)
 bool __intel_display_wa(struct intel_display *display, enum intel_display_wa wa, const char *name)
 {
 	switch (wa) {
-	case INTEL_DISPLAY_WA_13012396614:
-		return DISPLAY_VERx100(display) == 3000 ||
-			DISPLAY_VERx100(display) == 3500;
 	case INTEL_DISPLAY_WA_14011503117:
 		return DISPLAY_VER(display) == 13;
 	case INTEL_DISPLAY_WA_14025769978:
@@ -75,8 +72,6 @@ bool __intel_display_wa(struct intel_display *display, enum intel_display_wa wa,
 		return intel_display_needs_wa_16023588340(display);
 	case INTEL_DISPLAY_WA_16025573575:
 		return intel_display_needs_wa_16025573575(display);
-	case INTEL_DISPLAY_WA_14011503117:
-		return DISPLAY_VER(display) == 13;
 	case INTEL_DISPLAY_WA_22014263786:
 		return IS_DISPLAY_VERx100(display, 1100, 1400);
 	default:
