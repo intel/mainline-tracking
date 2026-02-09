7b1ac0058948a9d9f2680e0d2d3fd11fb219a330
Dapeng Mi <dapeng1.mi@linux.intel.com>
2026-02-09 16:35:14 +0800

    perf regs: Enable dumping of SIMD registers
    This patch adds support for dumping SIMD registers using the new
    PERF_SAMPLE_REGS_ABI_SIMD ABI.
    
    Currently, the XMM, YMM, ZMM, OPMASK, eGPRs, and SSP registers on x86
    platforms are supported with the PERF_SAMPLE_REGS_ABI_SIMD ABI.
    
    An example of the output is displayed below.
    
    Example:
    
     $perf record -e cycles:p -Iax,bx,r8,r16,r31,ssp,xmm,ymm,zmm,opmask ./test
     $perf report -D
     ... ...
     3342715685845 0x3afe8 [0xbc8]: PERF_RECORD_SAMPLE(IP, 0x1):
     27776/27776: 0xffffffff91d7c18f period: 10000 addr: 0
     ... intr regs: mask 0x18001010003 ABI 64-bit SIMD
     .... AX    0xffffed102de1a606
     .... BX    0xffffed102de1a606
     .... R8    0x0000000000000001
     .... R16   0x0000000000000000
     .... R31   0x0000000000000000
     .... SSP   0x0000000000000000
     ... SIMD ABI nr_vectors 32 vector_qwords 8 nr_pred 8 pred_qwords 1
     .... ZMM  [0] 0xd5e9d326758e6800
     .... ZMM  [0] 0x000055d3c2483250
     .... ZMM  [0] 0x0000000000000000
     .... ZMM  [0] 0x0000000000000000
     .... ZMM  [0] 0x0000000000000000
     .... ZMM  [0] 0x0000000000000000
     .... ZMM  [0] 0x0000000000000000
     .... ZMM  [0] 0x0000000000000000
     .... ZMM  [1] 0x00007f5901ffbb10
     .... ZMM  [1] 0x00007f5901ffbb10
     .... ZMM  [1] 0x0000000000000000
     .... ZMM  [1] 0x0000000000000000
     .... ZMM  [1] 0x0000000000000000
     .... ZMM  [1] 0x0000000000000000
     .... ZMM  [1] 0x0000000000000000
     .... ZMM  [1] 0x0000000000000000
     ... ...
     .... ZMM  [31] 0x0000000000000000
     .... ZMM  [31] 0x0000000000000000
     .... ZMM  [31] 0x0000000000000000
     .... ZMM  [31] 0x0000000000000000
     .... ZMM  [31] 0x0000000000000000
     .... ZMM  [31] 0x0000000000000000
     .... ZMM  [31] 0x0000000000000000
     .... ZMM  [31] 0x0000000000000000
     .... OPMASK[0] 0x0000000000100221
     .... OPMASK[1] 0x0000000000000020
     .... OPMASK[2] 0x000000007fffffff
     .... OPMASK[3] 0x0000000000000000
     .... OPMASK[4] 0x0000000000000000
     .... OPMASK[5] 0x0000000000000000
     .... OPMASK[6] 0x0000000000000000
     .... OPMASK[7] 0x0000000000000000
     ... ...
    
    Co-developed-by: Kan Liang <kan.liang@linux.intel.com>
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>
    Signed-off-by: Dapeng Mi <dapeng1.mi@linux.intel.com>

--- tools/perf/util/session.c
+++ tools/perf/util/session.c
@@ -979,6 +979,69 @@ static void regs_dump__printf(u64 mask, struct regs_dump *regs,
 	}
 }
 
+static void simd_regs_dump__printf(uint16_t e_machine, struct regs_dump *regs, bool intr)
+{
+	const char *name = "unknown";
+	u32 i, idx = 0;
+	uint16_t qwords;
+	int reg_c;
+
+	if (!(regs->abi & PERF_SAMPLE_REGS_ABI_SIMD))
+		return;
+
+	printf("... SIMD ABI nr_vectors %ld vector_qwords %ld nr_pred %ld pred_qwords %ld\n",
+	       regs->nr_vectors, regs->vector_qwords,
+	       regs->nr_pred, regs->pred_qwords);
+
+	for (reg_c = 0; reg_c < 64; reg_c++) {
+		if (intr) {
+			perf_intr_simd_reg_class_bitmap_qwords(e_machine, reg_c,
+							       &qwords, /*pred=*/false);
+		} else {
+			perf_user_simd_reg_class_bitmap_qwords(e_machine, reg_c,
+							       &qwords, /*pred=*/false);
+		}
+		if (regs->vector_qwords == qwords) {
+			name = perf_simd_reg_class_name(e_machine, reg_c, /*pred=*/false);
+			break;
+		}
+	}
+
+	for (i = 0; i < regs->nr_vectors; i++) {
+		if (regs->vector_qwords > 1) {
+			printf(".... %-5s[%d] 0x%016" PRIx64 "\n", name, i, regs->simd_data[idx++]);
+			printf(".... %-5s[%d] 0x%016" PRIx64 "\n", name, i, regs->simd_data[idx++]);
+		}
+		if (regs->vector_qwords > 3) {
+			printf(".... %-5s[%d] 0x%016" PRIx64 "\n", name, i, regs->simd_data[idx++]);
+			printf(".... %-5s[%d] 0x%016" PRIx64 "\n", name, i, regs->simd_data[idx++]);
+		}
+		if (regs->vector_qwords > 7) {
+			printf(".... %-5s[%d] 0x%016" PRIx64 "\n", name, i, regs->simd_data[idx++]);
+			printf(".... %-5s[%d] 0x%016" PRIx64 "\n", name, i, regs->simd_data[idx++]);
+			printf(".... %-5s[%d] 0x%016" PRIx64 "\n", name, i, regs->simd_data[idx++]);
+			printf(".... %-5s[%d] 0x%016" PRIx64 "\n", name, i, regs->simd_data[idx++]);
+		}
+	}
+
+	name = "unknown";
+	for (reg_c = 0; reg_c < 64; reg_c++) {
+		if (intr) {
+			perf_intr_simd_reg_class_bitmap_qwords(e_machine, reg_c,
+							       &qwords, /*pred=*/true);
+		} else {
+			perf_user_simd_reg_class_bitmap_qwords(e_machine, reg_c,
+							       &qwords, /*pred=*/true);
+		}
+		if (regs->pred_qwords == qwords) {
+			name = perf_simd_reg_class_name(e_machine, reg_c, /*pred=*/true);
+			break;
+		}
+	}
+	for (i = 0; i < regs->nr_pred; i++)
+		printf(".... %-5s[%d] 0x%016" PRIx64 "\n", name, i, regs->simd_data[idx++]);
+}
+
 static const char *regs_abi[] = {
 	[PERF_SAMPLE_REGS_ABI_NONE] = "none",
 	[PERF_SAMPLE_REGS_ABI_32] = "32-bit",
@@ -1018,6 +1081,7 @@ static void regs_user__printf(struct perf_sample *sample, uint16_t e_machine, ui
 
 	if (user_regs->regs)
 		regs__printf("user", user_regs, e_machine, e_flags);
+	simd_regs_dump__printf(e_machine, user_regs, /*intr=*/false);
 }
 
 static void regs_intr__printf(struct perf_sample *sample, uint16_t e_machine, uint32_t e_flags)
@@ -1031,6 +1095,7 @@ static void regs_intr__printf(struct perf_sample *sample, uint16_t e_machine, ui
 
 	if (intr_regs->regs)
 		regs__printf("intr", intr_regs, e_machine, e_flags);
+	simd_regs_dump__printf(e_machine, intr_regs, /*intr=*/true);
 }
 
 static void stack_user__printf(struct stack_dump *dump)
