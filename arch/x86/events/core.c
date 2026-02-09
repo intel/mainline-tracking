591b4778e16719da752b79658d41f6dbf20f86a4
Dapeng Mi <dapeng1.mi@linux.intel.com>
2026-02-09 15:20:43 +0800

    perf/x86: Support eGPRs sampling using sample_regs_* fields
    Support sampling of APX eGPRs (R16 ~ R31) via the sample_regs_* fields.
    
    To sample eGPRs, the sample_simd_regs_enabled field must be set. This
    allows the spare space (reclaimed from the original XMM space) in the
    sample_regs_* fields to be used for representing eGPRs.
    
    The perf_reg_value() function needs to check if the
    PERF_SAMPLE_REGS_ABI_SIMD flag is set first, and then determine whether
    to output eGPRs or legacy XMM registers to userspace.
    
    The perf_reg_validate() function first checks the simd_enabled argument
    to determine if the eGPRs bitmap is represented in sample_regs_* fields.
    It then validates the eGPRs bitmap accordingly.
    
    Currently, eGPRs sampling is only supported on the x86_64 architecture, as
    APX is only available on x86_64 platforms.
    
    APX eGPRs sampling will be enabled in a subsequent patch that sets
    PERF_PMU_CAP_SIMD_REGS.
    
    Suggested-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Co-developed-by: Kan Liang <kan.liang@linux.intel.com>
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>
    Signed-off-by: Dapeng Mi <dapeng1.mi@linux.intel.com>

--- arch/x86/events/core.c
+++ arch/x86/events/core.c
@@ -708,19 +708,22 @@ int x86_pmu_hw_config(struct perf_event *event)
 	}
 
 	if (event->attr.sample_type & (PERF_SAMPLE_REGS_INTR | PERF_SAMPLE_REGS_USER)) {
-		/*
-		 * Besides the general purpose registers, XMM registers may
-		 * be collected as well.
-		 */
-		if (event_has_extended_regs(event)) {
-			if (!(event->pmu->capabilities & PERF_PMU_CAP_EXTENDED_REGS))
-				return -EINVAL;
-			if (event->attr.sample_simd_regs_enabled)
-				return -EINVAL;
-		}
-
 		if (event_has_simd_regs(event)) {
+			u64 reserved = ~GENMASK_ULL(PERF_REG_MISC_MAX - 1, 0);
+
 			if (!(event->pmu->capabilities & PERF_PMU_CAP_SIMD_REGS))
+				return -EINVAL;
+			/*
+			 * The XMM space in the perf_event_x86_regs is reclaimed
+			 * for eGPRs and other general registers.
+			 */
+			if (((event->attr.sample_type & PERF_SAMPLE_REGS_INTR) &&
+			     (event->attr.sample_regs_intr & reserved)) ||
+			    ((event->attr.sample_type & PERF_SAMPLE_REGS_USER) &&
+			     (event->attr.sample_regs_user & reserved)))
+				return -EINVAL;
+			if (event_needs_egprs(event) &&
+			    !(x86_pmu.ext_regs_mask & XFEATURE_MASK_APX))
 				return -EINVAL;
 			/* The vector registers set is not supported */
 			if (event_needs_xmm(event) &&
@@ -738,6 +741,15 @@ int x86_pmu_hw_config(struct perf_event *event)
 			if (event_needs_opmask(event) &&
 			    !(x86_pmu.ext_regs_mask & XFEATURE_MASK_OPMASK))
 				return -EINVAL;
+		} else {
+			/*
+			 * Besides the general purpose registers, XMM registers may
+			 * be collected as well.
+			 */
+			if (event_has_extended_regs(event)) {
+				if (!(event->pmu->capabilities & PERF_PMU_CAP_EXTENDED_REGS))
+					return -EINVAL;
+			}
 		}
 	}
 
@@ -1786,6 +1798,7 @@ void x86_pmu_clear_perf_regs(struct pt_regs *regs)
 	perf_regs->zmmh_regs = NULL;
 	perf_regs->h16zmm_regs = NULL;
 	perf_regs->opmask_regs = NULL;
+	perf_regs->egpr_regs = NULL;
 }
 
 static void update_perf_regs(struct x86_perf_regs *perf_regs,
@@ -1809,6 +1822,8 @@ static void update_perf_regs(struct x86_perf_regs *perf_regs,
 		perf_regs->h16zmm = get_xsave_addr(xsave, XFEATURE_Hi16_ZMM);
 	if (mask & XFEATURE_MASK_OPMASK)
 		perf_regs->opmask = get_xsave_addr(xsave, XFEATURE_OPMASK);
+	if (mask & XFEATURE_MASK_APX)
+		perf_regs->egpr = get_xsave_addr(xsave, XFEATURE_APX);
 }
 
 /*
@@ -1998,6 +2013,8 @@ static void x86_pmu_sample_xregs(struct perf_event *event,
 		mask |= XFEATURE_MASK_Hi16_ZMM;
 	if (event_needs_opmask(event))
 		mask |= XFEATURE_MASK_OPMASK;
+	if (event_needs_egprs(event))
+		mask |= XFEATURE_MASK_APX;
 
 	mask &= x86_pmu.ext_regs_mask;
 	if ((sample_type & PERF_SAMPLE_REGS_USER) && data->regs_user.regs) {
