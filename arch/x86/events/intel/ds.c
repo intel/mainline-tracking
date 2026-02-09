53d46eeeeba160d13018bd54a65d12e0d5dcf609
Dapeng Mi <dapeng1.mi@linux.intel.com>
2026-02-09 15:20:35 +0800

    perf/x86: Enable XMM Register Sampling for Non-PEBS Events
    Previously, XMM register sampling was only available for PEBS events
    starting from Icelake. Currently the support is now extended to non-PEBS
    events by utilizing the xsaves instruction, thereby completing the
    feature set.
    
    To implement this, a 64-byte aligned buffer is required. A per-CPU
    ext_regs_buf is introduced to store SIMD and other registers, with an
    approximate size of 2K. The buffer is allocated using kzalloc_node(),
    ensuring natural and 64-byte alignment for all kmalloc() allocations
    with powers of 2.
    
    XMM sampling for non-PEBS events is supported in the REGS_INTR case.
    Support for REGS_USER will be added in a subsequent patch. For PEBS
    events, XMM register sampling data is directly retrieved from PEBS
    records.
    
    Future support for additional vector registers (YMM/ZMM/OPMASK) is
    planned. An `ext_regs_mask` is added to track the supported vector
    register groups.
    
    Co-developed-by: Kan Liang <kan.liang@linux.intel.com>
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>
    Signed-off-by: Dapeng Mi <dapeng1.mi@linux.intel.com>

--- arch/x86/events/intel/ds.c
+++ arch/x86/events/intel/ds.c
@@ -1738,8 +1738,7 @@ static u64 pebs_update_adaptive_cfg(struct perf_event *event)
 	if (gprs || (attr->precise_ip < 2) || tsx_weight)
 		pebs_data_cfg |= PEBS_DATACFG_GP;
 
-	if ((sample_type & PERF_SAMPLE_REGS_INTR) &&
-	    (attr->sample_regs_intr & PERF_REG_EXTENDED_MASK))
+	if (event_has_extended_regs(event))
 		pebs_data_cfg |= PEBS_DATACFG_XMMS;
 
 	if (sample_type & PERF_SAMPLE_BRANCH_STACK) {
@@ -2513,6 +2512,7 @@ static void setup_pebs_adaptive_sample_data(struct perf_event *event,
 	struct pebs_meminfo *meminfo = NULL;
 	struct pebs_gprs *gprs = NULL;
 	struct x86_perf_regs *perf_regs;
+	u64 ignore_mask = 0;
 	u64 format_group;
 	u16 retire;
 
@@ -2520,7 +2520,7 @@ static void setup_pebs_adaptive_sample_data(struct perf_event *event,
 		return;
 
 	perf_regs = container_of(regs, struct x86_perf_regs, regs);
-	perf_regs->xmm_regs = NULL;
+	x86_pmu_clear_perf_regs(regs);
 
 	format_group = basic->format_group;
 
@@ -2567,6 +2567,7 @@ static void setup_pebs_adaptive_sample_data(struct perf_event *event,
 	if (format_group & PEBS_DATACFG_XMMS) {
 		struct pebs_xmm *xmm = next_record;
 
+		ignore_mask |= XFEATURE_MASK_SSE;
 		next_record = xmm + 1;
 		perf_regs->xmm_regs = xmm->xmm;
 	}
@@ -2605,6 +2606,8 @@ static void setup_pebs_adaptive_sample_data(struct perf_event *event,
 		next_record += nr * sizeof(u64);
 	}
 
+	x86_pmu_update_perf_regs(event, data, regs, ignore_mask);
+
 	WARN_ONCE(next_record != __pebs + basic->format_size,
 			"PEBS record size %u, expected %llu, config %llx\n",
 			basic->format_size,
@@ -2630,6 +2633,7 @@ static void setup_arch_pebs_sample_data(struct perf_event *event,
 	struct arch_pebs_aux *meminfo = NULL;
 	struct arch_pebs_gprs *gprs = NULL;
 	struct x86_perf_regs *perf_regs;
+	u64 ignore_mask = 0;
 	void *next_record;
 	void *at = __pebs;
 
@@ -2637,7 +2641,7 @@ static void setup_arch_pebs_sample_data(struct perf_event *event,
 		return;
 
 	perf_regs = container_of(regs, struct x86_perf_regs, regs);
-	perf_regs->xmm_regs = NULL;
+	x86_pmu_clear_perf_regs(regs);
 
 	__setup_perf_sample_data(event, iregs, data);
 
@@ -2692,6 +2696,7 @@ again:
 
 		next_record += sizeof(struct arch_pebs_xer_header);
 
+		ignore_mask |= XFEATURE_MASK_SSE;
 		xmm = next_record;
 		perf_regs->xmm_regs = xmm->xmm;
 		next_record = xmm + 1;
@@ -2739,6 +2744,8 @@ again:
 		at = at + header->size;
 		goto again;
 	}
+
+	x86_pmu_update_perf_regs(event, data, regs, ignore_mask);
 }
 
 static inline void *
@@ -3401,7 +3408,11 @@ static void __init intel_ds_pebs_init(void)
 				x86_pmu.flags |= PMU_FL_PEBS_ALL;
 				x86_pmu.pebs_capable = ~0ULL;
 				pebs_qual = "-baseline";
-				x86_get_pmu(smp_processor_id())->capabilities |= PERF_PMU_CAP_EXTENDED_REGS;
+				if (boot_cpu_has(X86_FEATURE_XSAVES)) {
+					x86_pmu.ext_regs_mask |= XFEATURE_MASK_SSE;
+					x86_get_pmu(smp_processor_id())->capabilities |=
+							PERF_PMU_CAP_EXTENDED_REGS;
+				}
 			} else {
 				/* Only basic record supported */
 				x86_pmu.large_pebs_flags &=
