4c462fafdca3f1e7471e3e9b8f08a0c2e2914d6c
Dapeng Mi <dapeng1.mi@linux.intel.com>
2026-02-09 15:20:39 +0800

    perf/x86: Enable YMM sampling using sample_simd_vec_reg_* fields
    This patch introduces support for sampling YMM registers via the
    sample_simd_vec_reg_* fields.
    
    Each YMM register consists of 4 u64 words, assembled from two halves:
    XMM (the lower 2 u64 words) and YMMH (the upper 2 u64 words). Although
    both XMM and YMMH data can be retrieved with a single xsaves instruction,
    they are stored in separate locations. The perf_simd_reg_value() function
    is responsible for assembling these halves into a complete YMM register
    for output to userspace.
    
    Additionally, sample_simd_vec_reg_qwords should be set to 4 to indicate
    YMM sampling.
    
    Co-developed-by: Kan Liang <kan.liang@linux.intel.com>
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>
    Signed-off-by: Dapeng Mi <dapeng1.mi@linux.intel.com>

--- arch/x86/events/core.c
+++ arch/x86/events/core.c
@@ -733,6 +733,9 @@ int x86_pmu_hw_config(struct perf_event *event)
 			if (event_needs_xmm(event) &&
 			    !(x86_pmu.ext_regs_mask & XFEATURE_MASK_SSE))
 				return -EINVAL;
+			if (event_needs_ymm(event) &&
+			    !(x86_pmu.ext_regs_mask & XFEATURE_MASK_YMM))
+				return -EINVAL;
 		}
 	}
 
@@ -1777,6 +1780,7 @@ void x86_pmu_clear_perf_regs(struct pt_regs *regs)
 
 	perf_regs->abi = PERF_SAMPLE_REGS_ABI_NONE;
 	perf_regs->xmm_regs = NULL;
+	perf_regs->ymmh_regs = NULL;
 }
 
 static void update_perf_regs(struct x86_perf_regs *perf_regs,
@@ -1792,6 +1796,8 @@ static void update_perf_regs(struct x86_perf_regs *perf_regs,
 
 	if (mask & XFEATURE_MASK_SSE)
 		perf_regs->xmm_space = xsave->i387.xmm_space;
+	if (mask & XFEATURE_MASK_YMM)
+		perf_regs->ymmh = get_xsave_addr(xsave, XFEATURE_YMM);
 }
 
 /*
@@ -1968,6 +1974,8 @@ static void x86_pmu_sample_xregs(struct perf_event *event,
 
 	if (event_needs_xmm(event))
 		mask |= XFEATURE_MASK_SSE;
+	if (event_needs_ymm(event))
+		mask |= XFEATURE_MASK_YMM;
 
 	mask &= x86_pmu.ext_regs_mask;
 	if ((sample_type & PERF_SAMPLE_REGS_USER) && data->regs_user.abi)
