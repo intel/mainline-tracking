ce592a4d6a3ea6991242445856e4737543e65465
Dapeng Mi <dapeng1.mi@linux.intel.com>
2026-02-09 15:20:39 +0800

    perf/x86: Support YMM sampling using sample_simd_vec_reg_* fields
    Support sampling of YMM registers via the sample_simd_vec_reg_* fields.
    
    Each YMM register consists of 4 u64 words, assembled from two halves:
    XMM (the lower 2 u64 words) and YMMH (the upper 2 u64 words). Although
    both XMM and YMMH data can be retrieved with a single xsaves instruction,
    they are stored in separate locations. The perf_simd_reg_value() function
    is responsible for assembling these halves into a complete YMM register
    for output to userspace.
    
    Additionally, sample_simd_vec_reg_qwords should be set to 4 to indicate
    YMM sampling.
    
    YMM sampling will be enabled in a subsequent patch that sets
    PERF_PMU_CAP_SIMD_REGS.
    
    Co-developed-by: Kan Liang <kan.liang@linux.intel.com>
    Signed-off-by: Kan Liang <kan.liang@linux.intel.com>
    Signed-off-by: Dapeng Mi <dapeng1.mi@linux.intel.com>

--- arch/x86/kernel/perf_regs.c
+++ arch/x86/kernel/perf_regs.c
@@ -77,6 +77,8 @@ u64 perf_reg_value(struct pt_regs *regs, int idx)
 	return regs_get_register(regs, pt_regs_offset[idx]);
 }
 
+#define PERF_X86_YMMH_QWORDS	(PERF_X86_YMM_QWORDS / 2)
+
 u64 perf_simd_reg_value(struct pt_regs *regs, int idx,
 			u16 qwords_idx, bool pred)
 {
@@ -98,6 +100,11 @@ u64 perf_simd_reg_value(struct pt_regs *regs, int idx,
 			return 0;
 		return perf_regs->xmm_regs[idx * PERF_X86_XMM_QWORDS +
 					   qwords_idx];
+	} else if (qwords_idx < PERF_X86_YMM_QWORDS) {
+		if (!perf_regs->ymmh_regs)
+			return 0;
+		return perf_regs->ymmh_regs[idx * PERF_X86_YMMH_QWORDS +
+					    qwords_idx - PERF_X86_XMM_QWORDS];
 	}
 
 	return 0;
@@ -121,7 +128,8 @@ int perf_simd_reg_validate(u64 sample_type, u16 simd_enabled,
 		if (vec_mask_intr || vec_mask_user)
 			return -EINVAL;
 	} else {
-		if (vec_qwords != PERF_X86_XMM_QWORDS)
+		if (vec_qwords != PERF_X86_XMM_QWORDS &&
+		    vec_qwords != PERF_X86_YMM_QWORDS)
 			return -EINVAL;
 		if (!vec_mask_intr && !vec_mask_user)
 			return -EINVAL;
