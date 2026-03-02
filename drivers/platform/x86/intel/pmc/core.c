43fa137362bebbc7858f4c654012e455d5ea8235
Xi Pardee <xi.pardee@linux.intel.com>
2026-03-02 14:32:07 -0800

    platform/x86/intel/pmc: Retrieve PMC info only for available PMCs
    Update the Intel PMC Core driver to fetch PMC information only for
    available PMCs. Previously, the driver attempted to retrieve PMC info
    even when the corresponding PMC was not present.
    
    This change aligns with recent updates to the Intel SSRAM Telemetry
    driver. Starting with NVL, the SSRAM Telemetry driver is probed for
    each individual SSRAM device. The prior implementation could not
    differentiate between an unavailable PMC and one that had not yet
    completed information retrieval. To resolve this, the PMC Core driver
    now skips obtaining PMC info for unavailable PMCs.
    
    Signed-off-by: Xi Pardee <xi.pardee@linux.intel.com>

--- drivers/platform/x86/intel/pmc/core.c
+++ drivers/platform/x86/intel/pmc/core.c
@@ -1745,16 +1745,19 @@ static int pmc_core_pmc_add(struct pmc_dev *pmcdev, unsigned int pmc_idx)
 	return 0;
 }
 
-static int pmc_core_ssram_get_reg_base(struct pmc_dev *pmcdev)
+static int pmc_core_ssram_get_reg_base(struct pmc_dev *pmcdev, u8 *pmc_list)
 {
+	unsigned int i;
 	int ret;
 
-	ret = pmc_core_pmc_add(pmcdev, PMC_IDX_MAIN);
-	if (ret)
-		return ret;
-
-	pmc_core_pmc_add(pmcdev, PMC_IDX_IOE);
-	pmc_core_pmc_add(pmcdev, PMC_IDX_PCH);
+	for (i = 0; pmc_list[i] != PMC_IDX_MAX; ++i) {
+		if (pmc_list[i] == PMC_IDX_MAIN) {
+			ret = pmc_core_pmc_add(pmcdev, pmc_list[i]);
+			if (ret)
+				return ret;
+		} else
+			pmc_core_pmc_add(pmcdev, pmc_list[i]);
+	}
 
 	return 0;
 }
@@ -1776,7 +1779,7 @@ int generic_core_init(struct pmc_dev *pmcdev, struct pmc_dev_info *pmc_dev_info)
 	ssram = pmc_dev_info->regmap_list != NULL;
 	if (ssram) {
 		pmcdev->regmap_list = pmc_dev_info->regmap_list;
-		ret = pmc_core_ssram_get_reg_base(pmcdev);
+		ret = pmc_core_ssram_get_reg_base(pmcdev, pmc_dev_info->pmc_list);
 		/*
 		 * EAGAIN error code indicates Intel PMC SSRAM Telemetry driver
 		 * has not finished probe and PMC info is not available yet. Try
