ef6fa974e47ad8a2db8fa84e79ac020e3e6a227c
Balamurugan C <balamurugan.c@intel.com>
2026-03-20 17:01:10 +0530

    Added spi_set_cs() for more stable r/w operations in SPI mode 3
    Signed-off-by: Balamurugan C <balamurugan.c@intel.com>

--- drivers/spi/spi.c
+++ drivers/spi/spi.c
@@ -1059,7 +1059,7 @@ static void spi_toggle_csgpiod(struct spi_device *spi, u8 idx, bool enable, bool
 		spi_delay_exec(&spi->cs_inactive, NULL);
 }
 
-static void spi_set_cs(struct spi_device *spi, bool enable, bool force)
+void spi_set_cs(struct spi_device *spi, bool enable, bool force)
 {
 	bool activate = enable;
 	u8 idx;
@@ -1116,6 +1116,7 @@ static void spi_set_cs(struct spi_device *spi, bool enable, bool force)
 			spi_delay_exec(&spi->cs_inactive, NULL);
 	}
 }
+EXPORT_SYMBOL_GPL(spi_set_cs);
 
 #ifdef CONFIG_HAS_DMA
 static int spi_map_buf_attrs(struct spi_controller *ctlr, struct device *dev,
