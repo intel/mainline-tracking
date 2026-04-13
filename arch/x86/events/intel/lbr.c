29ce83d3b6e5333ae339bd4fe71a437d6865fb58
Dapeng Mi <dapeng1.mi@linux.intel.com>
2026-04-13 13:35:52 +0800

    perf/x86/intel: Fix kernel address leakages in LBR stack
    Prior to the arch-LBR which supports CPL filtering, the kernel address
    could be leaked to user space even PERF_SAMPLE_BRANCH_USER is required.
    
    e.g., run below command on Intel Tigerlake platform,
    
    ```
    $./perf record -e cycles:p -o - --branch-filter any,save_type,u -- \
     	./perf bench syscall basic --loop 1000 | \
    	./perf script -i - --fields brstack|tr ' ' '\n'| \
    	grep -E '0x[89a-f][0-9a-f]{15}'
    
        Total time: 0.000 [sec]
    
          0.219000 usecs/op
         4,566,210 ops/sec
    [ perf record: Woken up 1 times to write data ]
    [ perf record: Captured and wrote 0.551 MB - ]
    0xffffffff93c001c8/0x7f12a2b1d647/P/-/-/16959/SYSRET/-
    0xffffffff93c001c8/0x7f12a2b1d5c2/P/-/-/17535/SYSRET/-
    0xffffffff93c01928/0x7f12a2861000/P/-/-/6719/ERET/-
    0xffffffff93c01928/0x7f12a297a000/P/-/-/8575/ERET/-
    ```
    The SYSRET/ERET branch calls are found the in the LBR stack, whose "from"
    addresses are obviously kernel address.
    
    Currently intel_pmu_lbr_filter() only filters out the LBR entries whose
    "to" address is a kernel address but doesn't check the "from" address.
    
    To fix the issue, extend the software filtering to both "from" and "to"
    addresses.
    
    Cc: stable@vger.kernel.org
    Reported-by: Ian Rogers <irogers@google.com>
    Fixes: 47125db27e47 ("perf/x86/intel/lbr: Support Architectural LBR")
    Signed-off-by: Dapeng Mi <dapeng1.mi@linux.intel.com>

--- arch/x86/events/intel/lbr.c
+++ arch/x86/events/intel/lbr.c
@@ -1212,7 +1212,7 @@ intel_pmu_lbr_filter(struct cpu_hw_events *cpuc)
 {
 	u64 from, to;
 	int br_sel = cpuc->br_sel;
-	int i, j, type, to_plm;
+	int i, j, type, to_plm, from_plm;
 	bool compress = false;
 
 	/* if sampling all branches, then nothing to filter */
@@ -1244,8 +1244,15 @@ intel_pmu_lbr_filter(struct cpu_hw_events *cpuc)
 				type |= X86_BR_NO_TX;
 		}
 
-		/* if type does not correspond, then discard */
-		if ((type & ~X86_BR_PLM) == X86_BR_NONE || (br_sel & type) != type) {
+		from_plm = kernel_ip(from) ? X86_BR_KERNEL : X86_BR_USER;
+		/*
+		 * If type does not correspond, then discard.
+		 * Especially filter out the entries whose from or to address
+		 * is a kernel address while only X86_BR_USER is set. This prevents
+		 * kernel address from being leaked into a user-space-only LBR stack.
+		 */
+		if ((type & ~X86_BR_PLM) == X86_BR_NONE || (br_sel & type) != type ||
+		    (!(br_sel & X86_BR_KERNEL) && (from_plm & X86_BR_KERNEL))) {
 			cpuc->lbr_entries[i].from = 0;
 			compress = true;
 		}
