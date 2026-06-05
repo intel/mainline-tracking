3a0ab6a88eed13fe75a1389ab6681aeb49f6bd5f
Dapeng Mi <dapeng1.mi@linux.intel.com>
2026-06-05 14:37:21 +0800

    perf/core: Check kernel access when kernel callchains are requested
    perf_event_open() currently gates perf_allow_kernel() only on
    !attr.exclude_kernel.
    
    However, users can still request kernel callchain collection with
    attr.exclude_callchain_kernel == 0 even when attr.exclude_kernel == 1.
    That still requires kernel profiling privilege, but the existing check
    does not enforce it.
    
    Update the permission check to call perf_allow_kernel() when either
    kernel sampling is requested or kernel callchains are requested.
    
    This keeps permission checks aligned with requested data and prevents
    unprivileged use of kernel callchain capture.
    
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Ingo Molnar <mingo@redhat.com>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Arnaldo Carvalho de Melo <acme@kernel.org>
    Cc: Namhyung Kim <namhyung@kernel.org>
    Cc: Ian Rogers <irogers@google.com>
    Signed-off-by: Dapeng Mi <dapeng1.mi@linux.intel.com>

--- kernel/events/core.c
+++ kernel/events/core.c
@@ -13941,7 +13941,9 @@ SYSCALL_DEFINE5(perf_event_open,
 	if (err)
 		return err;
 
-	if (!attr.exclude_kernel) {
+	if (!attr.exclude_kernel ||
+	    ((attr.sample_type & PERF_SAMPLE_CALLCHAIN) &&
+	     !attr.exclude_callchain_kernel)) {
 		err = perf_allow_kernel();
 		if (err)
 			return err;
