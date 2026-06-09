f57a3a6d7d89f056cb9a11c80f3eaff8ea649e30
Balamurugan C <balamurugan.c@intel.com>
2026-06-09 14:59:45 +0530

    ASoC: Intel: soc-acpi: Add entry for HDMI_In capture support in NVL match table
    Adding HDMI-In capture via I2S feature support in NVL platform.
    
    Signed-off-by: Balamurugan C <balamurugan.c@intel.com>

--- sound/soc/intel/common/soc-acpi-intel-nvl-match.c
+++ sound/soc/intel/common/soc-acpi-intel-nvl-match.c
@@ -15,7 +15,19 @@ static const struct snd_soc_acpi_codecs nvl_essx_83x6 = {
 	.codecs = { "ESSX8316", "ESSX8326", "ESSX8336"},
 };
 
+static const struct snd_soc_acpi_codecs nvl_lt6911_hdmi = {
+	.num_codecs = 1,
+	.codecs = {"INTC10B0"}
+};
+
 struct snd_soc_acpi_mach snd_soc_acpi_intel_nvl_machines[] = {
+	{
+		.comp_ids = &nvl_essx_83x6,
+		.drv_name = "nvl_es83x6_c1_h02",
+		.machine_quirk = snd_soc_acpi_codec_list,
+		.quirk_data = &nvl_lt6911_hdmi,
+		.sof_tplg_filename = "sof-nvl-es83x6-ssp1-hdmi-ssp02.tplg",
+	},
 	{
 		.comp_ids = &nvl_essx_83x6,
 		.drv_name = "sof-essx8336",
