ecad9f269d0e3df6a257140a5e4c0565e15b05f2
Abdul halim, Mohd Syazwan <mohd.syazwan.abdul.halim@intel.com>
2026-08-05 01:32:47 +0800

    drm/xe/xe3: Apply Wa_16029380221 to media
    Apply Wa_16029380221 to Xe3p_LPM.
    
    The Xe3p_LPM media page walker is hard-wired NonCoherent and cannot
    observe CPU:WB cached page table data. Force page tables to CPU:WC
    by clearing has_cached_pt when MEDIA_VERSION(3500) is detected.
    
    v2: Simplify code comment to avoid duplicating information
        already present in xe_wa_oob.rules. (Gustavo)
    
    Cc: Matt Roper <matthew.d.roper@intel.com>
    Reviewed-by: Gustavo Sousa <gustavo.sousa@intel.com>
    Signed-off-by: Nitin Gote <nitin.r.gote@intel.com>

--- drivers/gpu/drm/xe/xe_device.c
+++ drivers/gpu/drm/xe/xe_device.c
@@ -874,6 +874,15 @@ int xe_device_probe(struct xe_device *xe)
 			return err;
 	}
 
+	/*
+	 * Wa_16029380221: The affected GT will always use non-coherent
+	 * access to page tables, so we must do uncached writes from the
+	 * CPU.
+	 */
+	for_each_gt(gt, xe, id)
+		if (XE_GT_WA(gt, 16029380221))
+			xe->info.has_cached_pt = false;
+
 	for_each_tile(tile, xe, id) {
 		err = xe_ggtt_init_early(tile->mem.ggtt);
 		if (err)
